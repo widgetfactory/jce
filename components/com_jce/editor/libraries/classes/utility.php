bc9f67ad72ddbab8fba1aee5fa6409aff548a057
Ryan <ryandemmer@gmail.com>
2016-08-18 14:48:58 +0100

    Fix transliterator_transliterate error when processing arrays

--- components/com_jce/editor/libraries/classes/utility.php
+++ components/com_jce/editor/libraries/classes/utility.php
@@ -93,6 +93,14 @@ abstract class WFUtility
 
     private static function utf8_latin_to_ascii($subject)
     {
+        if (is_array($subject)) {
+            array_walk($subject, function(&$string) {
+              $string = WFUtility::utf8_latin_to_ascii($string);
+            });
+
+            return $subject;
+        }
+
         return transliterator_transliterate('Any-Latin; Latin-ASCII;', $subject);
     }
 
@@ -103,9 +111,9 @@ abstract class WFUtility
         }
 
         if (is_array($string)) {
-            foreach ($string as $value) {
-                $value = self::changeCase($value, $case);
-            }
+          array_walk($string, function(&$value, $key, $case) {
+            $value = WFUtility::changeCase($value, $case);
+          }, $case);
         } else {
             switch ($case) {
                 case 'lowercase':
