6758f6316603cfbe5e98f04fea737d53767e9c63
Ryan <ryandemmer@gmail.com>
2016-09-13 18:01:10 +0100

    Clean up

--- administrator/components/com_jce/views/cpanel/tmpl/default.php
+++ administrator/components/com_jce/views/cpanel/tmpl/default.php
@@ -12,42 +12,35 @@
 defined('_JEXEC') or die('RESTRICTED');
 ?>
 <div class="ui-jce">
-    <ul id="cpanel" class="thumbnails">
+    <ul id="cpanel" class="unstyled thumbnails">
       <?php echo implode("\n", $this->icons); ?>
     </ul>
-    <ul class="adminformlist">
-        <li>
-            <span class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_SUPPORT') . '::' . WFText::_('WF_CPANEL_SUPPORT_DESC'); ?>">
-<?php echo WFText::_('WF_CPANEL_SUPPORT'); ?>
-            </span>
-            <a href="http://www.joomlacontenteditor.net/support" target="_new">www.joomlacontenteditor.com/support</a>
-        </li>
-
-        <li>
-            <span class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_LICENCE') . '::' . WFText::_('WF_CPANEL_LICENCE_DESC'); ?>">
-<?php echo WFText::_('WF_CPANEL_LICENCE'); ?>
-            </span>
-                <?php echo $this->model->getLicense(); ?>
-        </li>
-        <li>
-            <span class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_VERSION') . '::' . WFText::_('WF_CPANEL_VERSION_DESC'); ?>">
-<?php echo WFText::_('WF_CPANEL_VERSION'); ?>
-            </span>
-                <?php echo $this->version; ?>
-        </li>
-            <?php if ($this->params->get('feed', 0) || WFModel::authorize('preferences')) : ?>
-            <li>
-                <span class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_FEED') . '::' . WFText::_('WF_CPANEL_FEED_DESC'); ?>">
-    <?php echo WFText::_('WF_CPANEL_FEED'); ?>
-                </span>
-                <span style="display:inline-block;">
-    <?php if ($this->params->get('feed', 0)) : ?>
-                        <ul class="newsfeed"><li><?php echo WFText::_('WF_CPANEL_FEED_NONE'); ?></li></ul>
-                    <?php else : ?>
-                        <?php echo WFText::_('WF_CPANEL_FEED_DISABLED'); ?> :: <a id="newsfeed_enable" title="<?php echo WFText::_('WF_PREFERENCES'); ?>" href="#">[<?php echo WFText::_('WF_CPANEL_FEED_ENABLE'); ?>]</a>
-                    <?php endif; ?>
-                </span>
-            </li>
-<?php endif; ?>
-    </ul>
+    <dl class="dl-horizontal">
+        <dt class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_SUPPORT') . '::' . WFText::_('WF_CPANEL_SUPPORT_DESC'); ?>">
+            <?php echo WFText::_('WF_CPANEL_SUPPORT'); ?>
+        </dt>
+        <dd><a href="http://www.joomlacontenteditor.net/support" target="_new">www.joomlacontenteditor.com/support</a></dd>
+        <dt class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_LICENCE') . '::' . WFText::_('WF_CPANEL_LICENCE_DESC'); ?>">
+            <?php echo WFText::_('WF_CPANEL_LICENCE'); ?>
+        </dt>
+        <dd><?php echo $this->model->getLicense(); ?></dd>
+        <dt class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_VERSION') . '::' . WFText::_('WF_CPANEL_VERSION_DESC'); ?>">
+            <?php echo WFText::_('WF_CPANEL_VERSION'); ?>
+        </dt>
+        <dd><?php echo $this->version; ?></dd>
+        <?php if ($this->params->get('feed', 0) || WFModel::authorize('preferences')) : ?>
+            <dt class="wf-tooltip" title="<?php echo WFText::_('WF_CPANEL_FEED') . '::' . WFText::_('WF_CPANEL_FEED_DESC'); ?>">
+                <?php echo WFText::_('WF_CPANEL_FEED'); ?>
+            </dt>
+            <dd>
+            <?php if ($this->params->get('feed', 0)) : ?>
+                <ul class="unstyled newsfeed">
+                    <li><?php echo WFText::_('WF_CPANEL_FEED_NONE'); ?></li>
+                </ul>
+            <?php else : ?>
+                <?php echo WFText::_('WF_CPANEL_FEED_DISABLED'); ?> :: <a id="newsfeed_enable" title="<?php echo WFText::_('WF_PREFERENCES'); ?>" href="#">[<?php echo WFText::_('WF_CPANEL_FEED_ENABLE'); ?>]</a>
+            <?php endif; ?>
+            </dd>
+        <?php endif; ?>
+    </dl>
 </div>
