e6221e65bc2f9c38cbd5d9d0ac08a06dfeff30dd
Ryan <ryandemmer@gmail.com>
2016-10-06 10:03:02 +0100

    Add webgl support check, create elements for IE8

--- components/com_jce/editor/libraries/js/lib/support.js
+++ components/com_jce/editor/libraries/js/lib/support.js
@@ -1,12 +1,25 @@
 (function($) {
+    // create HTML5 elements for IE8
+    document.createElement('main');
+    document.createElement('nav');
+
     var $tmp = document.createElement('div');
 
+    var canvas = document.createElement('canvas');
+
     // check for canvas
-    $.support.canvas = !!document.createElement('canvas').getContext;
+    $.support.canvas = !!canvas.getContext;
+    // check for webgl
+    try {
+        $.support.webgl = !!(canvas.getContext("webgl") || canvas.getContext("experimental-webgl"));
+    } catch (e) {
+        $.support.webgl = false;
+    }
+
     // check for background size
-    $.support.backgroundSize = (function () {
+    $.support.backgroundSize = (function() {
         var s = false;
-        $.each(['backgroundSize', 'MozBackgroundSize', 'WebkitBackgroundSize', 'OBackgroundSize'], function () {
+        $.each(['backgroundSize', 'MozBackgroundSize', 'WebkitBackgroundSize', 'OBackgroundSize'], function() {
             if (typeof $tmp.style[this] !== 'undefined') {
                 s = true;
             }
@@ -18,7 +31,7 @@
     /* http://downloads.beninzambia.com/blog/acrobat_detection.js.txt
      * Modified for our purposes
      */
-    $.support.pdf = (function () {
+    $.support.pdf = (function() {
         try {
             // IE
             if (!$.support.cssFloat) {
@@ -30,17 +43,13 @@
                 try {
                     // AcroPDF.PDF is used by version 7 and later
                     control = new ActiveXObject('AcroPDF.PDF');
-                }
-                catch (e) {
-                }
+                } catch (e) {}
 
                 if (!control) {
                     try {
                         // PDF.PdfCtrl is used by version 6 and earlier
                         control = new ActiveXObject('PDF.PdfCtrl');
-                    }
-                    catch (e) {
-                    }
+                    } catch (e) {}
                 }
 
                 return control ? true : false;
@@ -63,9 +72,7 @@
                     return true;
                 }
             }
-        }
-        catch (e) {
-        }
+        } catch (e) {}
 
         return false;
     })();
@@ -76,7 +83,8 @@
      * Copyright (c) 2009-2011 Faruk Ates, Paul Irish, Alex Sexton
      */
     $.support.video = (function() {
-        var el = document.createElement('video'), o = {};
+        var el = document.createElement('video'),
+            o = {};
         // IE9 Running on Windows Server SKU can cause an exception to be thrown, bug #224
         try {
 
@@ -93,8 +101,7 @@
                 return o;
             }
 
-        } catch (e) {
-        }
+        } catch (e) {}
 
         return false;
     })();
@@ -105,7 +112,8 @@
      * Copyright (c) 2009-2011 Faruk Ates, Paul Irish, Alex Sexton
      */
     $.support.audio = (function() {
-        var el = document.createElement('audio'), o = {};
+        var el = document.createElement('audio'),
+            o = {};
         try {
             if (!!el.canPlayType) {
                 o.ogg = el.canPlayType('audio/ogg; codecs="vorbis"');
@@ -120,8 +128,7 @@
 
                 return o;
             }
-        } catch (e) {
-        }
+        } catch (e) {}
 
         return false;
     })();
