fa85edf28358f81321f63008670e77c7f7dee6a6
Ryan <ryandemmer@gmail.com>
2016-12-05 11:16:45 +0000

    Fix layout for "none" theme

--- components/com_jce/editor/tiny_mce/themes/none/editor_template.js
+++ components/com_jce/editor/tiny_mce/themes/none/editor_template.js
@@ -9,109 +9,131 @@
  */
 
 (function() {
-	var DOM = tinymce.DOM;
+    var DOM = tinymce.DOM;
 
-	tinymce.create('tinymce.themes.NoSkin', {
-		init : function(ed, url) {
-			var t = this, s = ed.settings;
+    tinymce.create('tinymce.themes.NoSkin', {
+        init: function(ed, url) {
+            var t = this,
+                s = ed.settings;
 
-			t.editor = ed;
-			
-			function grabContent() {
-				var n, or, r, se = ed.selection;
+            t.editor = ed;
 
-				// Add new hidden div and store away selection
-				n = ed.dom.add(ed.getBody(), 'div', {id : '_mcePaste', style : 'position:absolute;left:-1000px;top:-1000px'}, '<br mce_bogus="1" />').firstChild;
-				or = ed.selection.getRng();
+            function grabContent() {
+                var n, or, r, se = ed.selection;
 
-				// Move caret into hidden div
-				r = ed.getDoc().createRange();
-				r.setStart(n, 0);
-				r.setEnd(n, 0);
-				se.setRng(r);
+                // Add new hidden div and store away selection
+                n = ed.dom.add(ed.getBody(), 'div', { id: '_mcePaste', style: 'position:absolute;left:-1000px;top:-1000px' }, '<br mce_bogus="1" />').firstChild;
+                or = ed.selection.getRng();
 
-				// Wait a while and grab the pasted contents
-				window.setTimeout(function() {
-					var n = ed.dom.get('_mcePaste');
+                // Move caret into hidden div
+                r = ed.getDoc().createRange();
+                r.setStart(n, 0);
+                r.setEnd(n, 0);
+                se.setRng(r);
 
-					// Grab the HTML contents
-					h = n.innerHTML;
+                // Wait a while and grab the pasted contents
+                window.setTimeout(function() {
+                    var n = ed.dom.get('_mcePaste');
 
-					// Remove hidden div and restore selection
-					ed.dom.remove(n);
-					se.setRng(or);
-					
-					h = h.replace(/<\/?\w+[^>]*>/gi, '');
+                    // Grab the HTML contents
+                    h = n.innerHTML;
 
-					// Post process (DOM)
-					el = ed.dom.create('div', 0, h);
-		
-					// Remove empty spans
-					tinymce.each(ed.dom.select('span', el).reverse(), function(n) {
-						// If the element doesn't have any attributes remove it
-						if (ed.dom.getAttribs(n).length <= 1 && n.className === '')
-							return ed.dom.remove(n, 1);
-					});
-		
-					// Insert new trimmed hs into editor, serialize it first to remove any unwanted elements/attributes
-					ed.execCommand('mceInsertContent', false, ed.serializer.serialize(el, {getInner : 1}));
-				}, 0);
-			};
+                    // Remove hidden div and restore selection
+                    ed.dom.remove(n);
+                    se.setRng(or);
 
-			ed.onInit.add(function() {
-				ed.onBeforeExecCommand.add(function(ed, cmd, ui, val, o) {
-					o.terminate  = true;
-					
-					return false;
-				});
-				
-				ed.dom.loadCSS(url + "/skins/default/content.css");
-			});
-			
-			ed.onKeyDown.add(function(ed, e) {
-				if ((e.ctrlKey && e.keyCode == 86) || (e.shiftKey && e.keyCode == 45))
-					grabContent();
-			});
-			
-			ed.onKeyDown.add(function(ed, e) {
-				if ((e.ctrlKey && e.keyCode == 66) || (e.ctrlKey && e.keyCode == 73) || (e.ctrlKey && e.keyCode == 85))
-					return tinymce.dom.Event.cancel(e);
-			});
+                    h = h.replace(/<\/?\w+[^>]*>/gi, '');
 
-			DOM.loadCSS((s.editor_css ? ed.baseURI.toAbsolute(s.editor_css) : '') || url + "/skins/default/ui.css");
-		},
+                    // Post process (DOM)
+                    el = ed.dom.create('div', 0, h);
 
-		renderUI : function(o) {
-			var t = this, n = o.targetNode, ic, tb, ed = t.editor, cf = ed.controlManager, sc;
+                    // Remove empty spans
+                    tinymce.each(ed.dom.select('span', el).reverse(), function(n) {
+                        // If the element doesn't have any attributes remove it
+                        if (ed.dom.getAttribs(n).length <= 1 && n.className === '')
+                            return ed.dom.remove(n, 1);
+                    });
 
-			n = DOM.insertAfter(DOM.create('span', {id : ed.id + '_container', 'class' : 'mceEditor defaultNoSkin'}), n);
-			n = sc = DOM.add(n, 'table', {cellPadding : 0, cellSpacing : 0, 'class' : 'mceLayout'});
-			n = tb = DOM.add(n, 'tbody');
+                    // Insert new trimmed hs into editor, serialize it first to remove any unwanted elements/attributes
+                    ed.execCommand('mceInsertContent', false, ed.serializer.serialize(el, { getInner: 1 }));
+                }, 0);
+            };
 
-			// Create iframe container
-			n = DOM.add(tb, 'tr');
-			n = ic = DOM.add(DOM.add(n, 'td'), 'div', {'class' : 'mceIframeContainer'});
+            ed.onInit.add(function() {
+                ed.onBeforeExecCommand.add(function(ed, cmd, ui, val, o) {
+                    o.terminate = true;
 
-			// Create toolbar container
-			//n = DOM.add(DOM.add(tb, 'tr', {'class' : 'last'}), 'td', {'class' : 'mceToolbar mceLast', align : 'center'});
+                    return false;
+                });
 
-			return {
-				iframeContainer : ic,
-				editorContainer : ed.id + '_container',
-				sizeContainer : sc,
-				deltaHeight : -20
-			};
-		},
+                ed.dom.loadCSS(url + "/skins/default/content.css");
+            });
 
-		getInfo : function() {
-			return {
-				longname : 'Simple theme',
-				author : 'Moxiecode Systems AB',
-				authorurl : 'http://tinymce.moxiecode.com',
-				version : tinymce.majorVersion + "." + tinymce.minorVersion
-			};
-		}
-	});
+            ed.onKeyDown.add(function(ed, e) {
+                if ((e.ctrlKey && e.keyCode == 86) || (e.shiftKey && e.keyCode == 45))
+                    grabContent();
+            });
 
-	tinymce.ThemeManager.add('none', tinymce.themes.NoSkin);
+            ed.onKeyDown.add(function(ed, e) {
+                if ((e.ctrlKey && e.keyCode == 66) || (e.ctrlKey && e.keyCode == 73) || (e.ctrlKey && e.keyCode == 85))
+                    return tinymce.dom.Event.cancel(e);
+            });
+
+            // Load CSS
+            if (!ed.settings.compress.css) {
+                DOM.loadCSS(s.editor_css ? ed.documentBaseURI.toAbsolute(s.editor_css) : url + "/skins/default/ui.css");
+            }
+        },
+
+        renderUI: function(o) {
+            var t = this,
+                n = o.targetNode,
+                ic, tb, ed = t.editor,
+                cf = ed.controlManager,
+                sc;
+
+            n = p = DOM.create('div', {
+                role: 'application',
+                'aria-labelledby': ed.id + '_voice',
+                id: ed.id + '_parent',
+                'class': 'mceEditor defaultSkin' + (ed.settings.directionality == "rtl" ? ' mceRtl' : '')
+            });
+
+            DOM.add(n, 'span', {
+                'class': 'mceVoiceLabel',
+                'style': 'display:none;',
+                id: ed.id + '_voice'
+            }, ed.settings.aria_label);
+
+            n = sc = DOM.add(n, 'div', {
+                role: "presentation",
+                id: ed.id + '_tbl',
+                'class': 'mceLayout'
+            });
+
+            ic = DOM.add(n, 'div', { 'class': 'mceIframeContainer' });
+
+            n = o.targetNode;
+
+            DOM.insertAfter(p, n);
+
+            return {
+                iframeContainer: ic,
+                editorContainer: ed.id + '_parent',
+                sizeContainer: sc,
+                deltaHeight: -20
+            };
+        },
+
+        getInfo: function() {
+            return {
+                longname: 'Simple theme',
+                author: 'Moxiecode Systems AB',
+                authorurl: 'http://tinymce.moxiecode.com',
+                version: tinymce.majorVersion + "." + tinymce.minorVersion
+            };
+        }
+    });
+
+    tinymce.ThemeManager.add('none', tinymce.themes.NoSkin);
 })();
