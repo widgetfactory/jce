9421bb994d88e217278bb6d030b2b7c4c714a63d
Ryan <ryandemmer@gmail.com>
2017-01-26 16:46:47 +0000

    Update and prevent multiple processing on Enter

--- components/com_jce/editor/tiny_mce/plugins/visualchars/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/visualchars/editor_plugin.js
@@ -5,110 +5,145 @@
  *
  * License: http://www.tinymce.com/license
  */
-(function() {
+(function () {
     tinymce.create('tinymce.plugins.VisualChars', {
-        init: function(ed, url) {
-            var t = this;
-            
-            t.editor = ed;
-            
+        init: function (ed, url) {
+            var self = this;
+
+            self.editor = ed;
+
             // get state from cookie
             var state = tinymce.util.Cookie.get('wf_visualchars_state');
-            
+
             state = tinymce.is(state, 'string') ? parseFloat(state) : ed.getParam('visualchars_default_state', 0);
 
-            ed.onInit.add(function() {
+            ed.onInit.add(function () {
                 ed.controlManager.setActive('visualchars', state);
-                
-                t._toggleVisualChars(state);
+
+                self._toggleVisualChars(state);
             });
-            
+
             // Register buttons
             ed.addButton('visualchars', {
                 title: 'visualchars.desc',
                 cmd: 'mceVisualChars'
             });
-            
+
             // add trigger for nbsp button
-            ed.onExecCommand.add(function(ed, cmd, ui, v, o) {
+            ed.onExecCommand.add(function (ed, cmd, ui, v, o) {
                 if (cmd === "mceNonBreaking") {
-                    t._toggleVisualChars(state);
+                    self._toggleVisualChars(state);
                 }
             });
-            
+
             // Register commands
-            ed.addCommand('mceVisualChars', function() {
+            ed.addCommand('mceVisualChars', function () {
                 state = !state;
 
                 ed.controlManager.setActive('visualchars', state);
-                t._toggleVisualChars(state);
-                
+                self._toggleVisualChars(state);
+
                 if (ed.getParam('use_cookies', 1)) {
                     tinymce.util.Cookie.set('wf_visualchars_state', state ? 1 : 0);
                 }
-            }, t);
-            
-            ed.onKeyUp.add(function(ed, e) {
+            }, self);
+
+            ed.onKeyUp.add(function (ed, e) {
                 if (state) {
                     if (e.keyCode == 13) {
-                        t._toggleVisualChars(state);
+                        self._toggleVisualChars(state);
                     }
                 }
             });
-            
-            ed.onPreProcess.add(function(ed, o) {
+
+            ed.onPreProcess.add(function (ed, o) {
                 if (o.get) {
-                    t._toggleVisualChars(false, o.node);
+                    self._toggleVisualChars(false, o.node);
                 }
             });
-            
-            ed.onSetContent.add(function(ed, o) {
-                t._toggleVisualChars(state);
+
+            ed.onSetContent.add(function (ed, o) {
+                self._toggleVisualChars(state);
             });
         },
-        
-        getInfo: function() {
-            return {
-                longname: 'Visual characters',
-                author: 'Moxiecode Systems AB / Ryan Demmer',
-                authorurl: 'http://tinymce.moxiecode.com',
-                infourl: 'http://wiki.moxiecode.com/index.php/TinyMCE:Plugins/visualchars',
-                version: tinymce.majorVersion + "." + tinymce.minorVersion
-            };
-        },
-        
-        // Private methods
 
-        _toggleVisualChars: function(state, o) {
-            var t = this, ed = t.editor, nl, i, h, d = ed.getDoc(), b = o || ed.getBody(), nv, s = ed.selection, bo, div, node;
+        _toggleVisualChars: function (state, o) {
+            var self = this;
+            
+            var ed = this.editor, node, nodeList, i, body = ed.getBody(),
+                nodeValue, selection = ed.selection,
+                div, bookmark;
+            var charMap, visualCharsRegExp;
+
+            charMap = {
+                '\u00a0': 'nbsp',
+                '\u00ad': 'shy'
+            };
+
+            function wrapCharWithSpan(value) {
+                return '<span data-mce-bogus="1" class="mce-item-' + charMap[value] + '">' + value + '</span>';
+            }
+
+            function compileCharMapToRegExp() {
+                var key, regExp = '';
+
+                for (key in charMap) {
+                    regExp += key;
+                }
+
+                return new RegExp('[' + regExp + ']', 'g');
+            }
+
+            function compileCharMapToCssSelector() {
+                var key, selector = '';
+
+                for (key in charMap) {
+                    if (selector) {
+                        selector += ',';
+                    }
+
+                    selector += 'span.mce-item-' + charMap[key];
+                }
+
+                return selector;
+            }
+
+            function isNode(n) {
+                return n.nodeType === 3 && !ed.dom.getParent(n, '.mce-item-nbsp, .mce-item-shy');
+            }
+
+            visualCharsRegExp = compileCharMapToRegExp();
 
             if (state) {
-                nl = [];
-                tinymce.walk(b, function(n) {
-                    if (n.nodeType == 3 && n.nodeValue && /(\u00a0|&nbsp;)/.test(n.nodeValue)) 
-                        nl.push(n);
+                nodeList = [];
+                tinymce.walk(body, function (n) {
+                    if (isNode(n) && n.nodeValue && visualCharsRegExp.test(n.nodeValue)) {
+                        nodeList.push(n);
+                    }
                 }, 'childNodes');
-				
-                for (i = 0; i < nl.length; i++) {
-                    nv = nl[i].nodeValue;
-                    nv = nv.replace(/(\u00a0|&nbsp;)/g, '<span data-mce-bogus="1" class="mceItemHidden mceItemNbsp">$1</span>');
-					
-                    div = ed.dom.create('div', null, nv);
-                    while (node = div.lastChild)
-                        ed.dom.insertAfter(node, nl[i]);
 
-                    ed.dom.remove(nl[i]);
+                for (i = 0; i < nodeList.length; i++) {
+                    nodeValue = nodeList[i].nodeValue;
+                    nodeValue = nodeValue.replace(visualCharsRegExp, wrapCharWithSpan);
+
+                    div = ed.dom.create('div', null, nodeValue);
+
+                    while ((node = div.lastChild)) {
+                        ed.dom.insertAfter(node, nodeList[i]);
+                    }
+
+                    ed.dom.remove(nodeList[i]);
                 }
             } else {
-                nl = ed.dom.select('span.mceItemNbsp', b);
+                nodeList = ed.dom.select(compileCharMapToCssSelector(), body);
 
-                for (i = nl.length - 1; i >= 0; i--) {
-                    ed.dom.remove(nl[i], 1);
+                for (i = nodeList.length - 1; i >= 0; i--) {
+                    ed.dom.remove(nodeList[i], 1);
                 }
             }
         }
     });
-    
+
     // Register plugin
     tinymce.PluginManager.add('visualchars', tinymce.plugins.VisualChars);
 })();
