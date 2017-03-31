78e0dd55a0f58f36a86ca1e917b71e08f610906a
Ryan <ryandemmer@gmail.com>
2017-03-31 16:01:57 +0100

    Add extended element to custom elements if it is not a standard html element

--- components/com_jce/editor/tiny_mce/plugins/cleanup/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/cleanup/editor_plugin.js
@@ -16,6 +16,11 @@
         return str.split(delim || ',');
     }
 
+    // list of HTML tags
+    var tags = [
+        'a', 'abbr', 'acronym', 'address', 'applet', 'area', 'article', 'aside', 'audio', 'b', 'base', 'basefont', 'bdi', 'bdo', 'bgsound', 'big', 'blink', 'blockquote', 'body', 'br', 'button', 'canvas', 'caption', 'center', 'cite', 'code', 'col', 'colgroup', 'command', 'content', 'data', 'datalist', 'dd', 'del', 'details', 'dfn', 'dialog', 'dir', 'div', 'dl', 'dt', 'element', 'em', 'embed', 'fieldset', 'figcaption', 'figure', 'font', 'footer', 'form', 'frame', 'frameset', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'head', 'header', 'hgroup', 'hr', 'html', 'i', 'iframe', 'image', 'img', 'input', 'ins', 'isindex', 'kbd', 'keygen', 'label', 'legend', 'li', 'link', 'listing', 'main', 'map', 'mark', 'marquee', 'menu', 'menuitem', 'meta', 'meter', 'multicol', 'nav', 'nobr', 'noembed', 'noframes', 'noscript', 'object', 'ol', 'optgroup', 'option', 'output', 'p', 'param', 'picture', 'plaintext', 'pre', 'progress', 'q', 'rp', 'rt', 'rtc', 'ruby', 's', 'samp', 'script', 'section', 'select', 'shadow', 'slot', 'small', 'source', 'spacer', 'span', 'strike', 'strong', 'style', 'sub', 'summary', 'sup', 'table', 'tbody', 'td', 'template', 'textarea', 'tfoot', 'th', 'thead', 'time', 'title', 'tr', 'track', 'tt', 'u', 'ul', 'var', 'video', 'wbr', 'xmp'
+    ];
+
     var fontIconRe = /<([a-z0-9]+)([^>]+)class="([^"]*)(glyph|uk-)?(fa|icon)-([\w-]+)([^"]*)"([^>]*)>(&nbsp;|\u00a0)?<\/\1>/gi;
 
     var emptyRx = /<(ol|ul|sub|sup|blockquote|span|font|a|table|tbody|tr|strong|em|b|i)\b([^>]+)><\/\1>/gi;
@@ -49,6 +54,13 @@
                         elements['th'].paddEmpty = false;
                         elements['td'].paddEmpty = false;
                     }
+
+                    each(elements, function (v, k) {
+                        // custom element
+                        if (tinymce.inArray(tags, k) === -1) {
+                            ed.schema.addCustomElements(k);
+                        }
+                    });
                 }
 
                 // only if "Cleanup HTML" enabled
