c508ee19c79f992982c9cccff0f8a44c5dc53961
Ryan <ryandemmer@gmail.com>
2017-07-12 14:49:28 +0100

    Add file hash to preview image url to prevent caching

--- components/com_jce/editor/extensions/filesystem/joomla.php
+++ components/com_jce/editor/extensions/filesystem/joomla.php
@@ -423,6 +423,8 @@ class WFJoomlaFileSystem extends WFFileSystem
                 );
             }
 
+            $data['preview'] .= '?' . md5_file($path);
+
             return array_merge_recursive($data, $image);
         }
 
