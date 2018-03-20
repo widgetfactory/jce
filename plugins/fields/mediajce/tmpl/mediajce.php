213645f69ec850da8e38b0d30ec04d0290baa69d
Ryan Demmer <ryandemmer@gmail.com>
2018-03-20 15:04:15 +0000

    Add files for mediajce filed

---
+++ plugins/fields/mediajce/tmpl/mediajce.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package     JCE.Plugin
+ * @subpackage  Fields.MediaJce
+ *
+ * @copyright   Copyright (C) 2018 Ryan Demmer. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+defined('_JEXEC') or die;
+
+require_once(JPATH_PLUGINS . '/fields/media/tmpl/media.php');
