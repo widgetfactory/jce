2b1ddc12f26500550b53698ce964d01bb5543220
Ryan <ryan@widgetfactorylimited.co.uk>
2019-02-19 16:06:25 +0000

    Set default dimensions for placeholder

--- components/com_jce/editor/tiny_mce/plugins/media/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/media/editor_plugin.js
@@ -512,8 +512,8 @@
                 var style = Styles.parse(n.attr('style'));
 
                 // get width an height
-                var w = n.attr('width') || style.width || '';
-                var h = n.attr('height') || style.height || '';
+                var w = n.attr('width')     || style.width  || 384;
+                var h = n.attr('height')    || style.height || 216;
 
                 var type = n.attr('type');
 
@@ -654,8 +654,8 @@
 
             // Set data attribute and class
             placeholder.attr({
-                width: w,
-                height: h,
+                width   : w,
+                height  : h,
                 'class': classes.join(' '),
                 'data-mce-json': JSON.serialize(o)
             });
