cc3a77b539329c8a39311403fdc63bd67086b145
Ryan <ryan@widgetfactorylimited.co.uk>
2019-02-26 10:47:58 +0000

    Sanitize media path

--- plugins/fields/mediajce/tmpl/mediajce.php
+++ plugins/fields/mediajce/tmpl/mediajce.php
@@ -9,6 +9,8 @@
  */
 defined('_JEXEC') or die;
 
+jimport('joomla.filesystem.path');
+
 if ($field->value == '')
 {
 	return;
@@ -42,7 +44,30 @@ foreach ($value as $path)
 	if (!$path)
 	{
 		continue;
-    }
+	}
+
+	// remove some common characters
+	$path = preg_replace('#[\+\\\?\#%&<>"\'=\[\]\{\},;@\^\(\)£€$]#', '', $path);
+
+	// trim
+	$path = trim($path);
+
+	// check for valid path after clean
+	if (!$path)
+	{
+		continue;
+	}
+
+	// clean path
+	$path = JPath::clean($path);
+	
+	// create full path
+	$fullpath = JPATH_SITE . '/' . trim($path, '/');
+
+	// check path is valid
+	if (!is_file($fullpath)) {
+		continue;
+	}
 
     // set text as basename if not an image
     if (!$text && $type !== "images") {
