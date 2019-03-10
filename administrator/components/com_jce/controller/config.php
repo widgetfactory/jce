9133f360762c6d847be4f50ff8429587d212ecdf
Ryan <ryan@widgetfactorylimited.co.uk>
2019-03-10 20:02:16 +0000

    Use core controller function for saving config

--- administrator/components/com_jce/controller/config.php
+++ administrator/components/com_jce/controller/config.php
@@ -15,77 +15,8 @@ class JceControllerConfig extends JControllerForm
 	public function __construct($config = array())
 	{
 		parent::__construct($config);
+		
 		// return to control panel on cancel/close
 		$this->view_list = 'cpanel';
-	}
-
-	public function save($key = null, $urlVar = null)
-	{
-		JSession::checkToken() or jexit(JText::_('JINVALID_TOKEN'));
-
-		$app = JFactory::getApplication();
-
-		$data = $this->input->get('jform', array(), 'array');
-		$user = JFactory::getUser();
-
-		// Check if the user is authorised to do this.
-		if (!$user->authorise('core.admin', 'com_jce')) {
-			$app->enqueueMessage(JText::_('JERROR_ALERTNOAUTHOR'), 'error');
-			$app->redirect('index.php?option=com_jce');
-		}
-
-		$model = $this->getModel();
-
-        // Validate the posted data.
-		// Sometimes the form needs some posted data, such as for plugins and modules.
-		$form = $model->getForm($data, false);
-
-		if (!$form) {
-			$app->enqueueMessage($model->getError(), 'error');
-
-			return false;
-		}
-
-		$validData = $model->validate($form, $data);
-
-		if ($validData === false) {
-            // Get the validation messages.
-			$errors = $model->getErrors();
-
-			// Push up to three validation messages out to the user.
-			for ($i = 0, $n = count($errors); $i < $n && $i < 3; $i++) {
-				if ($errors[$i] instanceof \Exception) {
-					$app->enqueueMessage($errors[$i]->getMessage(), 'warning');
-				} else {
-					$app->enqueueMessage($errors[$i], 'warning');
-				}
-			}
-
-			$app->redirect('index.php?option=com_jce&view=config');
-		}
-
-		try {			
-			$model->save($data);
-		} catch (RuntimeException $e) {
-			// Save failed, go back to the screen and display a notice.
-			$app->enqueueMessage(JText::sprintf('JERROR_SAVE_FAILED', $e->getMessage()), 'error');
-			$app->redirect(JRoute::_('index.php?option=com_jce&view=config', false));
-		}
-
-		$this->setMessage(JText::_($prefix . ($recordId === 0 && $app->isClient('site') ? '_SUBMIT' : '') . '_SAVE_SUCCESS'));
-
-		switch ($this->getTask()) {
-			case 'apply':
-				$app->redirect('index.php?option=com_jce&view=config');
-				break;
-			case 'save':
-				$app->redirect('index.php?option=com_jce');
-				break;
-		}
-
-        // Invoke the postSave method to allow for the child class to access the model.
-		$this->postSaveHook($model, $validData);
-
-		return true;
 	}
 }
