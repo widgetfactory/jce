7bf41011ad29b0feacaf62100413a600d5198b52
Ryan <ryan@widgetfactorylimited.co.uk>
2019-04-04 16:50:42 +0100

    Fix display of some editor-xtd buttons that have not added a btn class value

--- plugins/editors/jce/jce.php
+++ plugins/editors/jce/jce.php
@@ -166,6 +166,15 @@ class plgEditorJCE extends JPlugin
                 $buttons = $this->_subject->getButtons($name, $buttons, $asset, $author);
             }
 
+            // fix some legacy buttons
+            array_walk($buttons, function($button) {
+                $cls = $button->get('class', '');
+
+                $cls .= ' btn';
+                
+                $button->set('class', trim($cls));
+            });
+
             return JLayoutHelper::render('joomla.editors.buttons', $buttons);
         }
     }
