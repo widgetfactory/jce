546e97ca5131f6a20ff88074d7239e90d27d44c1
Ryan <ryan@widgetfactorylimited.co.uk>
2019-04-19 09:44:50 +0100

    Set system default values for wordcount

--- components/com_jce/editor/tiny_mce/plugins/wordcount/config.php
+++ components/com_jce/editor/tiny_mce/plugins/wordcount/config.php
@@ -13,7 +13,7 @@ class WFWordcountPluginConfig
     public static function getConfig(&$settings)
     {
         $wf = WFApplication::getInstance();
-        $settings['wordcount_limit'] = $wf->getParam('editor.wordcount_limit', 0);
-        $settings['wordcount_alert'] = $wf->getParam('editor.wordcount_alert', 0);
+        $settings['wordcount_limit'] = $wf->getParam('editor.wordcount_limit', 0, 0);
+        $settings['wordcount_alert'] = $wf->getParam('editor.wordcount_alert', 0, 0);
     }
 }
