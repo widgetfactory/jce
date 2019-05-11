33f19892f1460ab7ddb2722d4fe7f3d12e3856f9
Ryan <ryan@widgetfactorylimited.co.uk>
2019-05-11 10:36:02 +0100

    Remove use of $this in static functions

--- components/com_jce/editor/extensions/links/joomlalinks/weblinks.php
+++ components/com_jce/editor/extensions/links/joomlalinks/weblinks.php
@@ -47,11 +47,13 @@ class JoomlalinksWeblinks extends JObject
 
     public function getLinks($args)
     {
+        $wf = WFEditorPlugin::getInstance();
         $items = array();
 
         if (!defined('JPATH_PLATFORM')) {
             require_once JPATH_SITE . '/includes/application.php';
         }
+
         require_once JPATH_SITE . '/components/com_weblinks/helpers/route.php';
 
         $language = '';
@@ -60,7 +62,7 @@ class JoomlalinksWeblinks extends JObject
             // Get all WebLink categories
             default:
             case 'categories':
-                $categories = WFLinkBrowser::getCategory('com_weblinks', 1, $this->get('category_alias', 1));
+                $categories = WFLinkBrowser::getCategory('com_weblinks', 1, $wf->getParam('joomlalinks.category_alias', 1));
 
                 foreach ($categories as $category) {
                     $url = '';
@@ -92,11 +94,11 @@ class JoomlalinksWeblinks extends JObject
                 break;
             // Get all links in the category
             case 'category':
-                $categories = WFLinkBrowser::getCategory('com_weblinks', $args->id, $this->get('category_alias', 1));
+                $categories = WFLinkBrowser::getCategory('com_weblinks', $args->id, $wf->getParam('joomlalinks.category_alias', 1));
 
                 if (count($categories)) {
                     foreach ($categories as $category) {
-                        $children = WFLinkBrowser::getCategory('com_weblinks', $category->id, $this->get('category_alias', 1));
+                        $children = WFLinkBrowser::getCategory('com_weblinks', $category->id, $wf->getParam('joomlalinks.category_alias', 1));
 
                         $url = '';
 
@@ -158,6 +160,8 @@ class JoomlalinksWeblinks extends JObject
 
     public static function getWeblinks($id)
     {
+        $wf = WFEditorPlugin::getInstance();
+        
         $db = JFactory::getDBO();
         $user = JFactory::getUser();
 
@@ -169,7 +173,7 @@ class JoomlalinksWeblinks extends JObject
 
         $case = '';
 
-        if ((int) $this->get('weblinks_alias', 1)) {
+        if ((int) $wf->getParam('joomlalinks.weblinks_alias', 1)) {
             //sqlsrv changes
             $case_when1 = ' CASE WHEN ';
             $case_when1 .= $dbquery->charLength('a.alias', '!=', '0');
