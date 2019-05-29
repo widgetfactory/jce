80dd7af03cb139c12ef421222ca09ce4fa7307cf
Ryan <ryan@widgetfactorylimited.co.uk>
2019-05-29 12:12:03 +0100

    Add noneditable as core plugin

--- components/com_jce/editor/libraries/classes/editor.php
+++ components/com_jce/editor/libraries/classes/editor.php
@@ -626,7 +626,7 @@ class WFEditor
                 });
 
                 // core plugins
-                $core = array('core', 'autolink', 'cleanup', 'code', 'format', 'importcss', 'colorpicker', 'upload', 'figure', 'ui');
+                $core = array('core', 'autolink', 'cleanup', 'code', 'format', 'importcss', 'colorpicker', 'upload', 'figure', 'ui', 'noneditable');
 
                 // load branding plugin
                 if (!WF_EDITOR_PRO) {
