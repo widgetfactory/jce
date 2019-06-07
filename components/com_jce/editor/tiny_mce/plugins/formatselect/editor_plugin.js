ddf62c7f30c0ad3f6c133865faa3af0cf9b19b23
Ryan <ryan@widgetfactorylimited.co.uk>
2019-06-07 10:21:28 +0100

    Skip internal tags for Format elements

--- components/com_jce/editor/tiny_mce/plugins/formatselect/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/formatselect/editor_plugin.js
@@ -51,7 +51,7 @@
             });
 
             function isFormat(n) {
-                return tinymce.inArray(nodes, n.nodeName) !== -1;
+                return n.className.indexOf('mce-item') === -1 && tinymce.inArray(nodes, n.nodeName) !== -1;
             }
 
             ed.onNodeChange.add(function (ed, cm, n) {
