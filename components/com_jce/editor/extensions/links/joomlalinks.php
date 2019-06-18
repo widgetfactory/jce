04c886399cacaefd4b50b58e8e80aff22b7bd285
Ryan <ryan@widgetfactorylimited.co.uk>
2019-06-18 10:25:41 +0100

    Fix weblinks check

--- components/com_jce/editor/extensions/links/joomlalinks.php
+++ components/com_jce/editor/extensions/links/joomlalinks.php
@@ -39,7 +39,7 @@ class WFLinkBrowser_Joomlalinks
                 }
 
                 // skip weblinks if it doesn't exist!
-                if ($name === 'weblinks' && !is_file(JPATH_SITE . '/components/com_weblinks/helpers/route.php')) {
+                if ($name === 'weblinks' && !JComponentHelper::isEnabled('com_' . $name)) {
                     continue;
                 }
 
