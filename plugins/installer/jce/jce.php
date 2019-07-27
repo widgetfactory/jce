4e7bcebe50329f6f5c7eac423868baa60da1697f
Ryan <ryan@widgetfactorylimited.co.uk>
2019-07-27 10:59:26 +0100

    Check subscription key validty if required

--- plugins/installer/jce/jce.php
+++ plugins/installer/jce/jce.php
@@ -1,6 +1,6 @@
 <?php
 /**
- *  @copyright Copyright (c)2016 Ryan Demmer
+ *  @copyright Copyright (c)2016 - 2019 Ryan Demmer
  *  @license GNU General Public License version 2, or later
  */
 defined('_JEXEC') or die;
@@ -37,13 +37,17 @@ class plgInstallerJce extends JPlugin
         JLoader::import('joomla.application.component.helper');
         $component = JComponentHelper::getComponent('com_jce');
 
+        // load plugin language for warning messages
+        JFactory::getLanguage()->load('plg_installer_jce', JPATH_ADMINISTRATOR);
+
         $key = $component->params->get('updates_key', '');
 
-        if (empty($key) && strpos($url, 'pkg_jce_pro') !== false) {
-            $language = JFactory::getLanguage();
-            $language->load('plg_installer_jce', JPATH_ADMINISTRATOR);
-
-            $app->enqueueMessage(JText::_('PLG_INSTALLER_JCE_KEY_WARNING'), 'notice');
+        // if no key is set...
+        if (empty($key)) {
+            // if we are attempting to update JCE Pro, display a notice message
+            if (strpos($url, 'pkg_jce_pro') !== false) {
+                $app->enqueueMessage(JText::_('PLG_INSTALLER_JCE_KEY_WARNING'), 'notice');
+            }
 
             return true;
         }
@@ -51,6 +55,22 @@ class plgInstallerJce extends JPlugin
         // Append the subscription key to the download URL
         $uri->setVar('key', $key);
         $url = $uri->toString();
+
+        // check validity of the key and display a message if it is invalid / expired
+        try
+        {
+            $tmpUri = clone $uri;
+
+            $tmpUri->setVar('task', 'update.validate');
+            $tmpUri->delVar('file');
+            $tmpUrl = $tmpUri->toString();
+            $response = JHttpFactory::getHttp()->get($tmpUrl, array());
+        } catch (RuntimeException $exception) {}
+
+        // invalid key, display a notice message
+        if (403 == $response->code) {
+            $app->enqueueMessage(JText::_('PLG_INSTALLER_JCE_KEY_INVALID'), 'notice');
+        }
 
         return true;
     }
