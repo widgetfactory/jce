b9c730eee5b584c879039c86c6d79a4564b65b8c
Ryan <ryan@widgetfactorylimited.co.uk>
2019-09-02 20:02:53 +0100

    Format

--- components/com_jce/editor/libraries/classes/language.php
+++ components/com_jce/editor/libraries/classes/language.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @copyright 	Copyright (c) 2009-2019 Ryan Demmer. All rights reserved
- * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @copyright     Copyright (c) 2009-2019 Ryan Demmer. All rights reserved
+ * @license       GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
@@ -17,7 +17,7 @@ abstract class WFLanguage
      */
     protected static function check($tag)
     {
-        return file_exists(JPATH_SITE.'/language/'.$tag.'/'.$tag.'.com_jce.ini');
+        return file_exists(JPATH_SITE . '/language/' . $tag . '/' . $tag . '.com_jce.ini');
     }
 
     /**
