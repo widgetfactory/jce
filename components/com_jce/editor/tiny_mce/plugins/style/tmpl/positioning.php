063c671e2569f60081b6dfac3b4b7e4d013b18fe
Ryan <ryan@widgetfactorylimited.co.uk>
2019-10-08 14:19:21 +0100

    Format html

--- components/com_jce/editor/tiny_mce/plugins/style/tmpl/positioning.php
+++ components/com_jce/editor/tiny_mce/plugins/style/tmpl/positioning.php
@@ -11,49 +11,49 @@
 defined('JPATH_PLATFORM') or die;
 ?>
 
-  <div class="uk-grid uk-grid-small">
-    <label for="positioning_type" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_POSITIONING_TYPE'); ?></label>
-    <div class="uk-form-controls uk-width-3-10">
-      <select id="positioning_type" class="uk-datalist"></select>
-    </div>
-    <label for="positioning_visibility" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_VISIBILITY'); ?></label>
-    <div class="uk-form-controls uk-width-3-10">
-      <select id="positioning_visibility" class="uk-datalist"></select>
-    </div>
+<div class="uk-grid uk-grid-small">
+  <label for="positioning_type" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_POSITIONING_TYPE'); ?></label>
+  <div class="uk-form-controls uk-width-3-10">
+    <select id="positioning_type" class="uk-datalist"></select>
   </div>
-
-  <div class="uk-grid uk-grid-small">
-    <label for="positioning_width" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_WIDTH'); ?></label>
-        <div class="uk-form-controls uk-width-2-10">
-          <input type="number" id="positioning_width" onchange="StyleDialog.synch('positioning_width','box_width');" />
-        </div>
-        <div class="uk-form-controls uk-width-2-10">
-          <select id="positioning_width_measurement" ></select>
-        </div>
-        <label for="positioning_zindex" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_ZINDEX'); ?></label>
-        <div class="uk-form-controls uk-width-2-10">
-          <input type="number" id="positioning_zindex" />
-        </div>
+  <label for="positioning_visibility" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_VISIBILITY'); ?></label>
+  <div class="uk-form-controls uk-width-3-10">
+    <select id="positioning_visibility" class="uk-datalist"></select>
   </div>
-
-  <div class="uk-grid uk-grid-small">
-    <label for="positioning_height" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_HEIGHT'); ?></label>
-        <div class="uk-form-controls uk-width-2-10">
-          <input type="number" id="positioning_height" onchange="StyleDialog.synch('positioning_height','box_height');" />
-        </div>
-        <div class="uk-form-controls uk-width-2-10">
-          <select id="positioning_height_measurement" ></select>
-        </div>
-        <label for="positioning_overflow" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_OVERFLOW'); ?></label>
-        <div class="uk-form-controls uk-width-2-10">
-          <select id="positioning_overflow" class="uk-datalist"></select>
-        </div>
-  </div>
+</div>
 
 <div class="uk-grid uk-grid-small">
-<div class="uk-width-5-10">
-  <fieldset>
-    <legend><?php echo JText::_('WF_STYLES_PLACEMENT'); ?></legend>
+  <label for="positioning_width" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_WIDTH'); ?></label>
+  <div class="uk-form-controls uk-width-2-10">
+    <input type="number" id="positioning_width" onchange="StyleDialog.synch('positioning_width','box_width');" />
+  </div>
+  <div class="uk-form-controls uk-width-2-10">
+    <select id="positioning_width_measurement"></select>
+  </div>
+  <label for="positioning_zindex" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_ZINDEX'); ?></label>
+  <div class="uk-form-controls uk-width-2-10">
+    <input type="number" id="positioning_zindex" />
+  </div>
+</div>
+
+<div class="uk-grid uk-grid-small">
+  <label for="positioning_height" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_HEIGHT'); ?></label>
+  <div class="uk-form-controls uk-width-2-10">
+    <input type="number" id="positioning_height" onchange="StyleDialog.synch('positioning_height','box_height');" />
+  </div>
+  <div class="uk-form-controls uk-width-2-10">
+    <select id="positioning_height_measurement"></select>
+  </div>
+  <label for="positioning_overflow" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_OVERFLOW'); ?></label>
+  <div class="uk-form-controls uk-width-2-10">
+    <select id="positioning_overflow" class="uk-datalist"></select>
+  </div>
+</div>
+
+<div class="uk-grid uk-grid-small">
+  <div class="uk-width-5-10">
+    <fieldset>
+      <legend><?php echo JText::_('WF_STYLES_PLACEMENT'); ?></legend>
 
 
       <div class="uk-form-row">
@@ -64,12 +64,12 @@ defined('JPATH_PLATFORM') or die;
         <label for="positioning_placement_left" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_TOP'); ?></label>
 
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_placement_top" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_placement_top_measurement" ></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_placement_top" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_placement_top_measurement"></select>
+        </div>
 
 
       </div>
@@ -77,12 +77,12 @@ defined('JPATH_PLATFORM') or die;
         <label for="positioning_placement_left" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_RIGHT'); ?></label>
 
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_placement_right" disabled="disabled" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_placement_right_measurement" disabled="disabled"></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_placement_right" disabled="disabled" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_placement_right_measurement" disabled="disabled"></select>
+        </div>
 
 
       </div>
@@ -90,12 +90,12 @@ defined('JPATH_PLATFORM') or die;
         <label for="positioning_placement_left" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_BOTTOM'); ?></label>
 
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_placement_bottom" disabled="disabled" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_placement_bottom_measurement" disabled="disabled"></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_placement_bottom" disabled="disabled" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_placement_bottom_measurement" disabled="disabled"></select>
+        </div>
 
 
       </div>
@@ -103,22 +103,22 @@ defined('JPATH_PLATFORM') or die;
         <label for="positioning_placement_left" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_LEFT'); ?></label>
 
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_placement_left" disabled="disabled" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_placement_left_measurement" disabled="disabled"></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_placement_left" disabled="disabled" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_placement_left_measurement" disabled="disabled"></select>
+        </div>
 
 
       </div>
 
-  </fieldset>
-</div>
+    </fieldset>
+  </div>
 
-<div class="uk-width-5-10">
-  <fieldset>
-    <legend><?php echo JText::_('WF_STYLES_CLIP'); ?></legend>
+  <div class="uk-width-5-10">
+    <fieldset>
+      <legend><?php echo JText::_('WF_STYLES_CLIP'); ?></legend>
 
 
       <div class="uk-form-row">
@@ -129,12 +129,12 @@ defined('JPATH_PLATFORM') or die;
         <label for="positioning_clip_top" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_TOP'); ?></label>
 
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_clip_top" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_clip_top_measurement" ></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_clip_top" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_clip_top_measurement"></select>
+        </div>
 
 
       </div>
@@ -142,12 +142,12 @@ defined('JPATH_PLATFORM') or die;
         <label for="positioning_clip_right" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_RIGHT'); ?></label>
 
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_clip_right" disabled="disabled" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_clip_right_measurement" disabled="disabled"></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_clip_right" disabled="disabled" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_clip_right_measurement" disabled="disabled"></select>
+        </div>
 
 
       </div>
@@ -155,28 +155,28 @@ defined('JPATH_PLATFORM') or die;
         <label for="positioning_clip_bottom" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_BOTTOM'); ?></label>
 
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_clip_bottom" disabled="disabled" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_clip_bottom_measurement" disabled="disabled"></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_clip_bottom" disabled="disabled" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_clip_bottom_measurement" disabled="disabled"></select>
+        </div>
 
 
       </div>
       <div class="uk-grid uk-grid-small">
         <label for="positioning_clip_left" class="uk-form-label uk-width-2-10"><?php echo JText::_('WF_STYLES_LEFT'); ?></label>
 
-            <div class="uk-form-controls uk-width-4-10">
-              <input type="number" id="positioning_clip_left" disabled="disabled" />
-            </div>
-            <div class="uk-form-controls uk-width-4-10">
-              <select id="positioning_clip_left_measurement" disabled="disabled"></select>
-            </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <input type="number" id="positioning_clip_left" disabled="disabled" />
+        </div>
+        <div class="uk-form-controls uk-width-4-10">
+          <select id="positioning_clip_left_measurement" disabled="disabled"></select>
+        </div>
 
 
       </div>
 
-  </fieldset>
-</div>
+    </fieldset>
+  </div>
 </div>
