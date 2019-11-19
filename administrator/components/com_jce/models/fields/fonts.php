2a50e261995f52ebd0ede92d0b9b68c52b2adb83
Ryan <ryan@widgetfactorylimited.co.uk>
2019-11-19 09:35:25 +0000

    Pass through font data as an array of the object to fix parameter merging

--- administrator/components/com_jce/models/fields/fonts.php
+++ administrator/components/com_jce/models/fields/fonts.php
@@ -22,12 +22,12 @@ class JFormFieldFonts extends JFormFieldCheckboxes
     protected $layout = 'form.field.fonts';
 
     /**
-	 * Flag to tell the field to always be in multiple values mode.
-	 *
-	 * @var    boolean
-	 * @since  11.1
-	 */
-	protected $forceMultiple = false;
+     * Flag to tell the field to always be in multiple values mode.
+     *
+     * @var    boolean
+     * @since  11.1
+     */
+    protected $forceMultiple = false;
 
     private static $fonts = array('Andale Mono' => 'andale mono,times', 'Arial' => 'arial,helvetica,sans-serif', 'Arial Black' => 'arial black,avant garde', 'Book Antiqua' => 'book antiqua,palatino', 'Comic Sans MS' => 'comic sans ms,sans-serif', 'Courier New' => 'courier new,courier', 'Georgia' => 'georgia,palatino', 'Helvetica' => 'helvetica', 'Impact' => 'impact,chicago', 'Symbol' => 'symbol', 'Tahoma' => 'tahoma,arial,helvetica,sans-serif', 'Terminal' => 'terminal,monaco', 'Times New Roman' => 'times new roman,times', 'Trebuchet MS' => 'trebuchet ms,geneva', 'Verdana' => 'verdana,geneva', 'Webdings' => 'webdings', 'Wingdings' => 'wingdings,zapf dingbats');
 
@@ -46,29 +46,37 @@ class JFormFieldFonts extends JFormFieldCheckboxes
     protected function getOptions()
     {
         $fieldname = preg_replace('/[^a-zA-Z0-9_\-]/', '_', $this->fieldname);
-        $options   = array();
+        $options = array();
 
-        $fonts = $this->value;
-
-        if (is_string($fonts)) {
-            $fonts = json_decode(htmlspecialchars_decode($fonts), true);
+        if (is_string($this->value)) {
+            $this->value = json_decode(htmlspecialchars_decode($this->value), true);
         }
 
         // cast to array
-        $fonts = (array) $fonts;
+        $this->value = (array) $this->value;
+
+        $fonts = array();
+
+        // map for new format, where fonts are saved as an array of an associative array, eg: [['Andale Mono' => 'andale mono,times', 'Arial' => 'arial,helvetica,sans-serif']]
+        foreach($this->value as $key => $value) {
+            if (is_numeric($key)) {
+                $fonts = $value;
+            } else {
+                $fonts[$key] = $value;
+            }
+        }
 
         // the full font list, including custom fonts
         $items = array_merge(self::$fonts, $fonts);
-        
-        foreach($items as $text => $value)
-        {                                   
+
+        foreach ($items as $text => $value) {
             $value = htmlspecialchars_decode($value, ENT_QUOTES);
-            
+
             $tmp = array(
-                'value'    => $value,
-                'text'     => JText::alt($text, $fieldname),
-                'checked'  => empty($fonts) ? true : in_array($value, array_values($fonts)),
-                'custom'   => !in_array($value, array_values(self::$fonts))
+                'value'     => $value,
+                'text'      => JText::alt($text, $fieldname),
+                'checked'   => empty($fonts) ? true : in_array($value, array_values($fonts)),
+                'custom'    => !in_array($value, array_values(self::$fonts)),
             );
 
             $options[] = (object) $tmp;
