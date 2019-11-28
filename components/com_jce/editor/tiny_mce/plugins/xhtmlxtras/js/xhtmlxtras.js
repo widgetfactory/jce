b0922285c72f9a7d95c4c474652317fc6131f4db
Ryan <ryan@widgetfactorylimited.co.uk>
2019-11-28 20:18:51 +0000

    Remove comments

--- components/com_jce/editor/tiny_mce/plugins/xhtmlxtras/js/xhtmlxtras.js
+++ components/com_jce/editor/tiny_mce/plugins/xhtmlxtras/js/xhtmlxtras.js
@@ -140,10 +140,10 @@
 				$('input.media').parents('.uk-form-row').hide();
 			}
 
-			/*$('.uk-form-controls select:not(.uk-datalist)').datalist({ 'input': false }).trigger('datalist:update');
+			$('.uk-form-controls select:not(.uk-datalist)').datalist({ 'input': false }).trigger('datalist:update');
 
 			// trigger datalist init/update
-			$('.uk-datalist').trigger('datalist:update');*/
+			$('.uk-datalist').trigger('datalist:update');
 		},
 
 		insert: function () {
