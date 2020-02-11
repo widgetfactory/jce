d8dbf208dc1c680e61bdb29597e7451879432f90
Ryan <ryan@widgetfactorylimited.co.uk>
2020-02-11 20:23:10 +0000

    Replace tinyMCEPopup with editor reference

--- components/com_jce/editor/libraries/js/lib/plugin.js
+++ components/com_jce/editor/libraries/js/lib/plugin.js
@@ -264,8 +264,8 @@
             var ed = tinyMCEPopup.editor;
 
             ed.windowManager.open({
-                url: tinyMCEPopup.getParam('site_url') + 'index.php?option=com_jce&view=help&tmpl=component&lang=' + ed.settings.language + '&section=editor&category=' + this.getName(),
-                title: tinyMCEPopup.getLang('dlg.help', 'Help'),
+                url: ed.getParam('site_url') + 'index.php?option=com_jce&view=help&tmpl=component&lang=' + ed.settings.language + '&section=editor&category=' + this.getName(),
+                title: ed.getLang('dlg.help', 'Help'),
                 width: 896,
                 height: 768,
                 size: 'mce-modal-landscape-full',
