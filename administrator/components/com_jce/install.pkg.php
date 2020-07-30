74feb5a9ba6fca8dcdc7ad48a3e6f272510b6e71
Ryan <ryan@widgetfactorylimited.co.uk>
2020-07-30 19:43:31 +0100

    Remove triggering of onExtensionAfterInstall event which was causing dlid to be removed

--- administrator/components/com_jce/install.pkg.php
+++ administrator/components/com_jce/install.pkg.php
@@ -250,10 +250,11 @@ class pkg_jceInstallerScript
     {
         $app = JFactory::getApplication();
         $extension = JTable::getInstance('extension');
+        $parent = $installer->getParent();
 
         JTable::addIncludePath(JPATH_ADMINISTRATOR . '/components/com_jce/tables');
 
-        $plugin = JPluginHelper::getPlugin('extension', 'joomla');
+        /*$plugin = JPluginHelper::getPlugin('extension', 'joomla');
 
         if ($plugin) {
             $parent = $installer->getParent();
@@ -274,7 +275,7 @@ class pkg_jceInstallerScript
                 // install
                 $app->triggerEvent('onExtensionAfterInstall', array($parent, $package_id));
             }
-        }
+        }*/
 
         // remove legacy jcefilebrowser quickicon
         $plugin = JPluginHelper::getPlugin('quickicon', 'jcefilebrowser');
