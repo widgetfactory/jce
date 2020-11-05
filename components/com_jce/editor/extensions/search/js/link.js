59bb9d967a36a14af3195183245269b52b28ea3b
Ryan <ryan@widgetfactorylimited.co.uk>
2020-11-05 13:21:46 +0000

    Refactor search routine

--- components/com_jce/editor/extensions/search/js/link.js
+++ components/com_jce/editor/extensions/search/js/link.js
@@ -8,21 +8,21 @@
  * other free or open source software licenses.
  */
 var WFLinkSearch = WFExtensions.add('LinkSearch', {
-	
-    options : {
-        element		    : '#search-input',
-        button          : '#search-button',
-        clear           : '#search-clear',
-        empty           : 'No Results',
-        onClick 	    : $.noop
+
+    options: {
+        element: '#search-input',
+        button: '#search-button',
+        clear: '#search-clear',
+        empty: 'No Results',
+        onClick: $.noop
     },
-	
-    init : function(options) {
+
+    init: function (options) {
         $.extend(this.options, options);
-        
+
         var self = this, el = this.options.element, btn = this.options.button;
-        
-        $(btn).on('click', function(e) {
+
+        $(btn).on('click', function (e) {
             self.search();
             e.preventDefault();
         }).button({
@@ -31,30 +31,30 @@ var WFLinkSearch = WFExtensions.add('LinkSearch', {
             }
         });
 
-        $('#search-clear').on('click', function(e) {
-            if($(this).hasClass('uk-active')) {
+        $('#search-clear').on('click', function (e) {
+            if ($(this).hasClass('uk-active')) {
                 $(this).removeClass('uk-active');
 
                 $(el).val('');
                 $('#search-result').empty().hide();
             }
         });
-        
-        $('#search-options-button').on('click', function(e) {
+
+        $('#search-options-button').on('click', function (e) {
             e.preventDefault();
 
             $(this).addClass('uk-active');
-            
+
             var $p = $('#search-options').parent();
-            
+
             $('#search-options').height($p.parent().height() - $p.outerHeight() - 15).toggle();
 
-        }).on('close', function() {
+        }).on('close', function () {
             $(this).removeClass('uk-active');
             $('#search-options').hide();
         });
 
-        $(el).on('change keyup', function() {
+        $(el).on('change keyup', function () {
             if (this.value === "") {
                 $('#search-result').empty().hide();
                 $('#search-clear').removeClass('uk-active');
@@ -62,59 +62,59 @@ var WFLinkSearch = WFExtensions.add('LinkSearch', {
         });
     },
 
-    search : function() {
+    search: function () {
         var self = this, s = this.options, el = s.element, btn = s.button, $p = $('#search-result').parent();
-   
+
         var query = $(el).val();
-        
+
         if (!query || $(el).hasClass('placeholder')) {
             return;
         }
         $('#search-clear').removeClass('uk-active');
         $('#search-browser').addClass('loading');
-        
+
         // clean query
         query = $.trim(query.replace(/[\///<>#]/g, ''));
 
         Wf.JSON.request('doSearch', {
-            'json' : [query]
-        }, function(o) {
+            'json': [query]
+        }, function (o) {
             if (o) {
-                if (!o.error) {
-                    
-                    $('#search-result').empty();
-                    
-                    if (o.length) {                        
-                        $.each(o, function(i, n) {                        
-                            var $dl = $('<dl class="uk-margin-small" />').appendTo('#search-result');
-                            
-                            $('<dt class="link uk-margin-small" />').text(n.title).on('click', function() {
-                                if ($.isFunction(self.options.onClick)) {
-                                    self.options.onClick.call(this, Wf.String.decode(n.link));
-                                }
-                            }).prepend('<i class="uk-icon uk-icon-file-text-o uk-margin-small-right" />').appendTo($dl);
-                            
-                            $('<dd class="text">' + n.text + '</dd>').appendTo($dl);
-
-                            if (n.anchors) {
-                                $.each(n.anchors, function(i, a) {
-                                    $('<dd class="anchor"><i role="presentation" class="uk-icon uk-icon-anchor uk-margin-small-right"></i>#' + a + '</dd>').on('click', function() {
-                                        self.options.onClick.call(this, Wf.String.decode(n.link + '#' + a));
-                                    }).appendTo($dl);
-                                });
-                            }
-                        }); 
-                        
-                        $('dl:odd', '#search-result').addClass('odd');
-                        
-                    } else {
-                        $('#search-result').append('<p>' + s.empty + '</p>');
-                    }
-                    $('#search-options-button').trigger('close');
-                    $('#search-result').height($p.parent().height() - $p.outerHeight() - 5).show();
-                } else {
+                if (o.error) {
                     Wf.Dialog.alert(o.error);
+                    return;
                 }
+
+                $('#search-result').empty();
+
+                if (o.length) {
+                    $.each(o, function (i, n) {
+                        var $dl = $('<dl class="uk-margin-small" />').appendTo('#search-result');
+
+                        $('<dt class="link uk-margin-small" />').text(n.title).on('click', function () {
+                            if ($.isFunction(self.options.onClick)) {
+                                self.options.onClick.call(this, Wf.String.decode(n.link));
+                            }
+                        }).prepend('<i class="uk-icon uk-icon-file-text-o uk-margin-small-right" />').appendTo($dl);
+
+                        $('<dd class="text">' + n.text + '</dd>').appendTo($dl);
+
+                        if (n.anchors) {
+                            $.each(n.anchors, function (i, a) {
+                                $('<dd class="anchor"><i role="presentation" class="uk-icon uk-icon-anchor uk-margin-small-right"></i>#' + a + '</dd>').on('click', function () {
+                                    self.options.onClick.call(this, Wf.String.decode(n.link + '#' + a));
+                                }).appendTo($dl);
+                            });
+                        }
+                    });
+
+                    $('dl:odd', '#search-result').addClass('odd');
+
+                } else {
+                    $('#search-result').append('<p>' + s.empty + '</p>');
+                }
+                $('#search-options-button').trigger('close');
+                $('#search-result').height($p.parent().height() - $p.outerHeight() - 5).show();
             }
             $('#search-browser').removeClass('loading');
             $('#search-clear').addClass('uk-active');
