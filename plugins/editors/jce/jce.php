480cd9f9aa15b2c037a9c91eccd8137c7e5fedb2
Ryan <ryan@widgetfactorylimited.co.uk>
2021-06-01 14:43:08 +0100

    Load unique instances of the editor base on passed in config

--- plugins/editors/jce/jce.php
+++ plugins/editors/jce/jce.php
@@ -18,6 +18,8 @@ defined('JPATH_PLATFORM') or die;
  */
 class plgEditorJCE extends JPlugin
 {
+    protected static $instances = array();
+    
     /**
      * Constructor.
      *
@@ -27,31 +29,29 @@ class plgEditorJCE extends JPlugin
      * @since       1.5
      */
     public function __construct(&$subject, $config)
-    {
+    {        
         parent::__construct($subject, $config);
     }
 
     protected function getEditorInstance()
-    {
-        static $instance;
+    {        
+        // pass config to WFEditor
+        $config = array(
+            'profile_id' => $this->params->get('profile_id', 0),
+            'plugin' => $this->params->get('plugin', '')
+        );
 
-        if (!isset($instance)) {
+        $signature = md5(serialize($config));
+
+        if (empty(self::$instances[$signature])) {
             // load base file
             require_once JPATH_ADMINISTRATOR . '/components/com_jce/includes/base.php';
 
-            // pass config to WFEditor
-            $config = (array) $this->getProperties();
-            
-            // filter to only allow "id" or "plugin"
-            $config = array_filter($config, function($key) {
-                return $key == 'id' || $key == 'plugin';
-            }, ARRAY_FILTER_USE_KEY);
-
             // create editor
-            $instance = new WFEditor($config);
+            self::$instances[$signature] = new WFEditor($config);
         }
 
-        return $instance;
+        return self::$instances[$signature];
     }
 
     /**
@@ -118,22 +118,22 @@ class plgEditorJCE extends JPlugin
     }
 
     /**
-	 * JCE WYSIWYG Editor - Display the editor area.
-	 *
-	 * @param   string   $name     The name of the editor area.
-	 * @param   string   $content  The content of the field.
-	 * @param   string   $width    The width of the editor area.
-	 * @param   string   $height   The height of the editor area.
-	 * @param   int      $col      The number of columns for the editor area.
-	 * @param   int      $row      The number of rows for the editor area.
-	 * @param   boolean  $buttons  True and the editor buttons will be displayed.
-	 * @param   string   $id       An optional ID for the textarea. If not supplied the name is used.
-	 * @param   string   $asset    The object asset
-	 * @param   object   $author   The author.
-	 * @param   array    $params   Associative array of editor parameters.
-	 *
-	 * @return  string
-	 */
+     * JCE WYSIWYG Editor - Display the editor area.
+     *
+     * @param   string   $name     The name of the editor area.
+     * @param   string   $content  The content of the field.
+     * @param   string   $width    The width of the editor area.
+     * @param   string   $height   The height of the editor area.
+     * @param   int      $col      The number of columns for the editor area.
+     * @param   int      $row      The number of rows for the editor area.
+     * @param   boolean  $buttons  True and the editor buttons will be displayed.
+     * @param   string   $id       An optional ID for the textarea. If not supplied the name is used.
+     * @param   string   $asset    The object asset
+     * @param   object   $author   The author.
+     * @param   array    $params   Associative array of editor parameters.
+     *
+     * @return  string
+     */
     public function onDisplay($name, $content, $width, $height, $col, $row, $buttons = true, $id = null, $asset = null, $author = null, $params = array())
     {
         if (empty($id)) {
@@ -186,7 +186,7 @@ class plgEditorJCE extends JPlugin
 
             if (!empty($list)) {
                 $options = array(
-                    'joomla_xtd_buttons' => $list
+                    'joomla_xtd_buttons' => $list,
                 );
 
                 JFactory::getDocument()->addScriptOptions('plg_editor_jce', $options, true);
@@ -219,7 +219,7 @@ class plgEditorJCE extends JPlugin
 
         if (!empty($buttons)) {
             foreach ($buttons as $i => $button) {
-                if ($button->get('name')) {                    
+                if ($button->get('name')) {
                     // Set some vars
                     $name = 'button-' . $i . '-' . str_replace(' ', '-', $button->get('text'));
                     $title = $button->get('text');
