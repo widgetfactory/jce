9eb37bece6357ad0ad93881de3e90260990c0975
Ryan <ryan@widgetfactorylimited.co.uk>
2022-01-07 12:41:06 +0000

    Use default popup_src instead of defined template

--- components/com_jce/editor/libraries/views/popups/tmpl/popups.php
+++ components/com_jce/editor/libraries/views/popups/tmpl/popups.php
@@ -1,8 +1,8 @@
 <?php
 
 /**
- * @copyright 	Copyright (c) 2009-2021 Ryan Demmer. All rights reserved
- * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * @copyright     Copyright (c) 2009-2021 Ryan Demmer. All rights reserved
+ * @license       GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
  * JCE is free software. This version may have been modified pursuant
  * to the GNU General Public License, and as distributed it includes or
  * is derivative of works licensed under the GNU General Public License or
@@ -12,17 +12,32 @@ defined('JPATH_PLATFORM') or die;
 
 ?>
 <div class="uk-form-row uk-grid uk-grid-small uk-margin-small-bottom">
-<label for="popup_list" class="uk-form-label uk-width-1-5 hastip" title="<?php echo JText::_('WF_POPUP_TYPE_DESC'); ?>"><?php echo JText::_('WF_POPUP_TYPE'); ?></label>
+	<label for="popup_list" class="uk-form-label uk-width-1-5 hastip" title="<?php echo JText::_('WF_POPUP_TYPE_DESC'); ?>"><?php echo JText::_('WF_POPUP_TYPE'); ?></label>
 	<div class="uk-form-controls uk-width-2-5">
 		<?php echo $this->popups->getPopupList(); ?>
 	</div>
 </div>
 
-<div class="uk-form-row uk-grid uk-grid-small uk-margin-small-bottom" style="display:<?php echo ($this->popups->get('text') === false) ? 'none' : ''?>;">
-	<label for="popup_text" class="hastip uk-form-label uk-width-1-5"
-			title="<?php echo JText::_('WF_POPUP_TEXT_DESC'); ?>"><?php echo JText::_('WF_POPUP_TEXT'); ?></label>
-		<div class="uk-form-controls uk-width-4-5">
-			<input id="popup_text" type="text" value="" />
-		</div>
+<?php if ($this->popups->get('text')): ?>
+
+<div class="uk-form-row uk-grid uk-grid-small uk-margin-small-bottom"">
+	<label for=" popup_text" class="hastip uk-form-label uk-width-1-5" title="<?php echo JText::_('WF_POPUP_TEXT_DESC'); ?>"><?php echo JText::_('WF_POPUP_TEXT'); ?></label>
+	<div class="uk-form-controls uk-width-4-5">
+		<input id="popup_text" type="text" value="" />
+	</div>
 </div>
+
+<?php endif;?>
+
+<?php if ($this->popups->get('url')): ?>
+
+<div class="uk-form-row uk-margin-small-bottom uk-grid uk-grid-small">
+	<label for="popup_src" class="uk-form-label uk-width-1-5 hastip" title="<?php echo JText::_('WF_LABEL_URL_DESC'); ?>"><?php echo JText::_('WF_LABEL_URL'); ?></label>
+	<div class="uk-form-controls uk-width-4-5">
+		<input id="popup_src" type="text" value="" class="uk-input-multiple-disabled browser files" />
+	</div>
+</div>
+
+<?php endif;?>
+
 <?php echo $this->popups->getPopupTemplates(); ?>
