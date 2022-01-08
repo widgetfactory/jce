5c9869f1ce6b86e830520c84db603ad0778b098c
Ryan <ryan@widgetfactorylimited.co.uk>
2022-01-08 16:21:33 +0000

    Update styleformat.js

--- media/com_jce/js/lib/styleformat.js
+++ media/com_jce/js/lib/styleformat.js
@@ -42,7 +42,7 @@
             if ($('div.styleformat-list div.styleformat').length === 1) {
 
                 // clear inputs and remove styles
-                $(this).parents('.styleformat').find('input, select').val('').removeAttr('style').removeAttr('class');
+                $('input, select', this.parentNode).val("").removeAttr('style').removeAttr('class');
                 // hide
                 $(this).parents('.styleformat').hide();
                 // otherwise remove it
