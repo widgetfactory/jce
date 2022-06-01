b9822b21f129b0c942f4cdaf03b3ada84433cad1
Ryan <ryan@widgetfactorylimited.co.uk>
2022-06-01 11:44:56 +0100

    Validate "source" content from textarea on save

--- components/com_jce/editor/tiny_mce/plugins/source-core/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/source-core/editor_plugin.js
@@ -1,5 +1,7 @@
 (function () {
-    var DOM = tinymce.DOM;
+    var DOM = tinymce.DOM,
+        DomParser = tinymce.html.DomParser,
+        HtmlSerializer = tinymce.html.Serializer;
 
     tinymce.create('tinymce.plugins.SourcePlugin', {
         init: function (ed, url) {
@@ -51,6 +53,64 @@
                 }
             });
         },
+
+        validateContent: function (content) {
+            var ed = this.editor;
+
+            var args = {
+                "no_events": true,
+                "format": "raw"
+            };
+
+            // create new settings object
+            var settings = {};
+
+            // extend with editor settings
+            tinymce.extend(settings, ed.settings);
+
+            // set content    
+            args.content = content;
+
+            if (ed.settings.validate) {
+                // trigger cleanup etc in editor
+                args.format = "html";
+
+                // set a load flag so code is processed as code blocks
+                args.load = true;
+
+                // onBeforeGetContent
+                ed.onBeforeGetContent.dispatch(ed, args);
+
+                // allow all tags
+                settings.verify_html = false;
+
+                // no root blocks
+                settings.forced_root_block = false;
+
+                // must validate
+                settings.validate = true;
+
+                // create dom parser
+                var parser = new DomParser(settings, ed.schema);
+
+                // create html serializer
+                var serializer = new HtmlSerializer(settings, ed.schema);
+
+                // clean content
+                args.content = serializer.serialize(parser.parse(args.content), args);
+
+                args.get = true;
+
+                // onPostProcess
+                ed.onPostProcess.dispatch(ed, args);
+
+                // pass content
+                content = args.content;
+            }
+
+            return content;
+        },
+
         insertContent: function (v) {
             var ed = this.editor,
                 el = ed.getElement();
@@ -72,17 +132,28 @@
                 }
             }
         },
+
         getContent: function () {
-            var ed = this.editor;
-            return ed.getElement().value;
+            var content = this.editor.getElement().value;
+            return this.validateContent(content);
         },
+
         save: function () {
-            return this.getContent();
+            // get validated content
+            var content = this.getContent();
+
+            // pass to textarea
+            this.editor.getElement().value = content;
+
+            // return content
+            return content;
         },
+
         hide: function () {
             var ed = this.editor;
             DOM.hide(ed.getElement());
         },
+
         toggle: function () {
             var ed = this.editor;
             var element = ed.getElement(),
@@ -128,6 +199,10 @@
             if (!DOM.hasClass(container, 'mce-fullscreen')) {
                 DOM.setStyle(element, 'width', width);
             }
+        },
+
+        getSelection: function () {
+            return document.getSelection();
         },
 
         getCursorPos: function () {
