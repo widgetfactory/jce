358eea262b5e429d3f0c9ed776091a48ac63447b
Ryan <ryan@widgetfactorylimited.co.uk>
2022-06-14 13:17:52 +0100

    Add Word Count of selection and update Word Count plugin

--- components/com_jce/editor/tiny_mce/plugins/wordcount/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/wordcount/editor_plugin.js
@@ -9,82 +9,85 @@
  */
 
 (function () {
-    var VK = tinymce.VK, DOM = tinymce.DOM;
+    var DOM = tinymce.DOM, Delay = tinymce.util.Delay;
 
     tinymce.PluginManager.add('wordcount', function (ed, url) {
-        var self = this,
-            last = 0, block = 0;
+        var self = this;
 
         var countre = ed.getParam('wordcount_countregex', /[\w\u2019\x27\-\u00C0-\u1FFF]+/g); // u2019 == &rsquo; u00c0-u00ff extended latin chars with diacritical marks. exclude uc397 multiplication & u00f7 division
         var cleanre = ed.getParam('wordcount_cleanregex', /[0-9.(),;:!?%#$?\x27\x22_+=\\\/\-]*/g);
-        var update_rate = ed.getParam('wordcount_update_rate', 2000);
-        var update_on_delete = ed.getParam('wordcount_update_on_delete', false);
+        var update_rate = ed.getParam('wordcount_update_rate', 200);
         var target_id = ed.id + '_word_count';
 
         ed.onWordCount = new tinymce.util.Dispatcher(self);
 
-        function getCount() {
+        function processText(tx) {
             var tc = 0;
-            var tx = ed.getContent({
-                format: 'raw'
-            });
 
-            if (tx) {
-                tx = tx.replace(/\.\.\./g, ' '); // convert ellipses to spaces
-                tx = tx.replace(/<.[^<>]*?>/g, ' ').replace(/&nbsp;|&#160;/gi, ' '); // remove html tags and space chars
+            if (!tx) {
+                return tc;
+            }
 
-                // deal with html entities
-                tx = tx.replace(/(\w+)(&#?[a-z0-9]+;)+(\w+)/i, "$1$3").replace(/&.+?;/g, ' ');
-                tx = tx.replace(cleanre, ''); // remove numbers and punctuation
+            tx = tx.replace(/\.\.\./g, ' '); // convert ellipses to spaces
+            tx = tx.replace(/<.[^<>]*?>/g, ' ').replace(/&nbsp;|&#160;/gi, ' '); // remove html tags and space chars
 
-                var wordArray = tx.match(countre);
-                if (wordArray) {
-                    tc = wordArray.length;
-                }
+            // deal with html entities
+            tx = tx.replace(/(\w+)(&#?[a-z0-9]+;)+(\w+)/i, "$1$3").replace(/&.+?;/g, ' ');
+            tx = tx.replace(cleanre, ''); // remove numbers and punctuation
+
+            var wordArray = tx.match(countre);
+
+            if (wordArray) {
+                tc = wordArray.length;
             }
 
             return tc;
         }
 
-        function countChars(ed) {
-            var limit = parseInt(ed.getParam('wordcount_limit', 0), 10), showAlert = ed.getParam('wordcount_alert', 0);
+        function getCount() {
+            var tx = ed.getContent({
+                format: 'raw',
+                no_events: true // ??
+            });
 
-            // Keep multiple calls from happening at the same time
-            if (block) {
+            return processText(tx);
+        }
+
+        function countChars(tc) {
+
+            if (ed.destroyed) {
                 return;
             }
 
-            block = 1;
+            if (tc) {
+                DOM.setAttrib(target_id, 'title', ed.getLang('wordcount.selection', 'Selected Words:'));
+                return DOM.setHTML(target_id, tc.toString());
+            }
 
-            setTimeout(function () {
-                if (!ed.destroyed) {
-                    var tc = getCount(ed);
+            var limit = parseInt(ed.getParam('wordcount_limit', 0), 10), showAlert = ed.getParam('wordcount_alert', 0);
 
-                    // if a limit is set, set the count as words remaining
-                    if (limit) {
-                        tc = limit - tc;
+            tc = getCount();
 
-                        if (tc < 0) {
-                            DOM.addClass(target_id, 'mceWordCountLimit');
+            // if a limit is set, set the count as words remaining
+            if (limit) {
+                tc = limit - tc;
 
-                            if (showAlert) {
-                                ed.windowManager.alert(ed.getLang('wordcount.limit_alert', 'You have reached the word limit set for this content.'));
-                            }
+                if (tc < 0) {
+                    DOM.addClass(target_id, 'mceWordCountLimit');
 
-                        } else {
-                            DOM.removeClass(target_id, 'mceWordCountLimit');
-                        }
+                    if (showAlert) {
+                        ed.windowManager.alert(ed.getLang('wordcount.limit_alert', 'You have reached the word limit set for this content.'));
                     }
 
-                    DOM.setHTML(target_id, tc.toString());
-
-                    ed.onWordCount.dispatch(ed, tc);
-
-                    setTimeout(function () {
-                        block = 0;
-                    }, update_rate);
+                } else {
+                    DOM.removeClass(target_id, 'mceWordCountLimit');
                 }
-            }, 1);
+            }
+
+            DOM.setAttrib(target_id, 'title', ed.getLang('wordcount.words', 'Words:'));
+            DOM.setHTML(target_id, tc.toString());
+
+            ed.onWordCount.dispatch(ed, tc);
         }
 
         ed.onPostRender.add(function (ed, cm) {
@@ -95,39 +98,35 @@
                 var row = DOM.get(ed.id + '_path_row');
 
                 if (row) {
+                    var label = ed.getLang('wordcount.words', 'Words:');
+
                     DOM.add(row.parentNode, 'div', {
                         'class': 'mceWordCount'
-                    }, ed.getLang('wordcount.words', 'Words: ') + '<span id="' + target_id + '" class="mceText">0</span>');
+                    }, '<span title="' + label + '" id="' + target_id + '" class="mceText">0</span>');
                 }
             }
         });
 
-        ed.onInit.add(function (ed) {
-            ed.selection.onSetContent.add(function () {
-                countChars(ed);
-            });
+        var countAll = Delay.debounce(function () {
+            countChars();
+        }, update_rate);
 
-            countChars(ed);
-        });
+        var countSelection = Delay.debounce(function () {
+            var rng = ed.selection.getRng(), sel = ed.selection.getSel();
 
-        ed.onSetContent.add(function (ed) {
-            countChars(ed);
-        });
-
-        function checkKeys(key) {
-            return key !== last && (key === VK.ENTER || last === VK.SPACEBAR || checkDelOrBksp(last));
-        }
-
-        function checkDelOrBksp(key) {
-            return key === VK.DELETE || key === VK.BACKSPACE;
-        }
-
-        ed.onKeyUp.add(function (ed, e) {
-            if (checkKeys(e.keyCode) || update_on_delete && checkDelOrBksp(e.keyCode)) {
-                countChars(ed);
+            if (!rng.collapsed) {
+                var text = rng.text || (sel.toString ? sel.toString() : '');
+                var count = processText(text);
+                countChars(count);
+            } else {
+                countChars();
             }
+        }, update_rate);
 
-            last = e.keyCode;
-        });
+        // update on selection or content change
+        ed.onSelectionChange.add(countSelection);
+
+        // set total count when editor loads
+        ed.onInit.add(countAll);
     });
 })();
