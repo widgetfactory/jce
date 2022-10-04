4db0aaa2819f59eadae3500bcc9e5e6af96dab67
Ryan <ryan@widgetfactorylimited.co.uk>
2022-10-04 12:38:47 +0100

    Update core Code editor

--- components/com_jce/editor/tiny_mce/plugins/source-core/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/source-core/editor_plugin.js
@@ -1,187 +1,194 @@
 (function () {
     var DOM = tinymce.DOM,
-        DomParser = tinymce.html.DomParser,
-        HtmlSerializer = tinymce.html.Serializer;
+        Event = tinymce.dom.Event;
+
+    function debounce(callback, time) {
+        var timer, func;
+
+        func = function () {
+            var args = arguments;
+
+            clearTimeout(timer);
+
+            timer = setTimeout(function () {
+                callback.apply(this, args);
+            }, time);
+        };
+
+        func.stop = function () {
+            clearTimeout(timer);
+        };
+
+        return func;
+    }
+
+    /**
+     * Clean content of system elements
+     * @param {Object} ed 
+     * @param {String} content 
+     */
+    function cleanContent(ed, content) {
+        return content.replace(/<br data-mce-bogus="1"([^>]+)>/gi, '');
+    }
+
+    function hasCustomEditorInit(ed) {
+
+        // widgetkit
+        if (window.widgetkit && ed.id.indexOf('wk_') !== -1) {
+            return true;
+        }
+
+        // sp page builder
+        if (ed.id.indexOf('sppb-editor-') !== -1) {
+            return true;
+        }
+
+        return false;
+    }
 
     tinymce.create('tinymce.plugins.SourcePlugin', {
         init: function (ed, url) {
             var self = this;
             self.editor = ed;
 
-            if (ed.plugins.fullscreen) {
-                ed.onFullScreen.add(function (ed, state) {
-                    var element = ed.getElement();
+            ed.onSetContent.add(function (ed, o) {
+                self.setContent(ed.getContent(), true);
+            });
 
-                    // get tabs header
-                    var header = DOM.getPrev(element, '.wf-editor-header');
-
-                    // store the containerHeight as we go into fullscreen mode
-                    if (state) {
-                        var vp = DOM.getViewPort();
-                        // set height as viewport - header
-                        DOM.setStyle(element, 'height', vp.h - header.offsetHeight);
-                    } else {
-                        DOM.setStyle(element, 'height', ed.settings.container_height);
-                    }
-                });
-
-                ed.onFullScreenResize.add(function (ed, vp) {
-                    var element = ed.getElement();
-                    DOM.setStyle(element, 'height', vp.h);
-                });
+            function isEditorActive() {
+                return DOM.hasClass(ed.getElement(), 'wf-no-editor') == false;
             }
 
-            function isCodeEditorActive() {
-                return ed.getElement().style.display == 'block';
-            }
+            this.isActive = false;
 
             ed.onInit.add(function (ed) {
-                ed.onBeforeGetContent.add(function (ed, e) {
-                	if (isCodeEditorActive() == false) {
-                    	return;
-                	}
-                	
-                	// cancel events
-                	e.no_events = true;
-                });
-                
+                if (isEditorActive() == false) {
+                    return;
+                }
+
                 // get the stored active tab
-                var activeTab = sessionStorage.getItem('wf-editor-tabs-' + ed.id) || ed.settings.active_tab || '';
+                var activeTab = sessionStorage.getItem('wf-editor-tabs-' + ed.id) || ed.settings.active_tab || "";
 
                 if (activeTab === "wf-editor-source") {
                     // hide editor
-                    ed.hide();
-                    // show textarea
-                    DOM.show(ed.getElement());
+                    DOM.hide(ed.getContainer());
 
-                    var element = ed.getElement(),
-                        container = element.parentNode;
+                    // hide textarea
+                    DOM.hide(ed.getElement());
 
-                    // get width from setting or session data or editor textarea
-                    var width = ed.settings.container_width || sessionStorage.getItem('wf-editor-container-width');
-
-                    if (!DOM.hasClass(container, 'mce-fullscreen')) {
-                        DOM.setStyle(element, 'width', width);
-                    }
+                    // toggle source with delay (this seems to be necessary in SP Page Builder to prevent text block cloning...?)
+                    window.setTimeout(function () {
+                        self.toggle();
+                    }, 10);
                 }
             });
+
+            this.ControlManager = new tinymce.ControlManager(ed);
         },
 
-        validateContent: function (content) {
+        getSourceEditor: function () {
             var ed = this.editor;
+            var textarea = DOM.get(ed.id + '_editor_source_textarea');
 
-            var args = {
-                "no_events": true,
-                "format": "raw"
-            };
+            return textarea || null;
+        },
 
-            // create new settings object
-            var settings = {};
+        setContent: function (v) {
+            var editor = this.getSourceEditor();
 
-            // extend with editor settings
-            tinymce.extend(settings, ed.settings);
-
-            // set content    
-            args.content = content;
-
-            if (ed.settings.validate) {
-                // trigger cleanup etc in editor
-                args.format = "html";
-
-                // set a load flag so code is processed as code blocks
-                args.load = true;
-
-                // onBeforeGetContent
-                ed.onBeforeGetContent.dispatch(ed, args);
-
-                // allow all tags
-                settings.verify_html = false;
-
-                // no root blocks
-                settings.forced_root_block = false;
-
-                // must validate
-                settings.validate = true;
-
-                // create dom parser
-                var parser = new DomParser(settings, ed.schema);
-
-                // create html serializer
-                var serializer = new HtmlSerializer(settings, ed.schema);
-
-                // clean content
-                args.content = serializer.serialize(parser.parse(args.content), args);
-
-                args.get = true;
-
-                // onPostProcess
-                ed.onPostProcess.dispatch(ed, args);
-
-                // pass content
-                content = args.content;
+            if (editor) {
+                return editor.value = v;
             }
 
-            return content;
+            return false;
         },
 
         insertContent: function (v) {
-            var ed = this.editor,
-                el = ed.getElement();
+            var editor = this.getSourceEditor();
 
-            // IE
-            if (document.selection) {
-                el.focus();
-                var s = document.selection.createRange();
-                s.text = v;
-                // Mozilla / Netscape
-            } else {
-                if (el.selectionStart || el.selectionStart == '0') {
-                    var startPos = el.selectionStart;
-                    var endPos = el.selectionEnd;
-                    el.value = el.value.substring(0, startPos) + v + el.value.substring(endPos, el.value.length);
-                    // Other
+            if (editor) {
+                editor.focus();
+
+                // IE
+                if (document.selection) {
+                    var rng = document.selection.createRange();
+                    rng.text = v;
+                    // Mozilla / Netscape
                 } else {
-                    el.value += v;
+                    editor.setRangeText(v, editor.selectionStart, editor.selectionEnd, "end");
                 }
             }
+
+            return false;
         },
 
         getContent: function () {
-            var ed = this.editor, args = {};
+            var editor = this.getSourceEditor();
 
-            args.content = ed.getElement().value;
+            // iframe has been created and source editor is visible
+            if (editor) {
+                return editor.value;
+            }
 
-            ed.onWfEditorSave.dispatch(ed, args);
-
-            return args.content;
-        },
-
-        save: function () {
-            // get validated content
-            var content = this.getContent();
-
-            // pass to textarea
-            this.editor.getElement().value = content;
-
-            // return content
-            return content;
+            return null;
         },
 
         hide: function () {
             var ed = this.editor;
-            DOM.hide(ed.getElement());
+            DOM.hide(ed.id + '_editor_source');
+        },
+
+        save: function (content, debounced) {
+            var ed = this.editor,
+                el = ed.getElement();
+
+            if (!el) {
+                return content;
+            }
+
+            // get content from source editor if not passed in
+            content = tinymce.is(content) ? content : this.getContent();
+
+            var args = {
+                content: content,
+                no_events: true,
+                format: 'raw'
+            };
+
+            ed.onWfEditorSave.dispatch(ed, args);
+
+            // pass to textarea
+            if (!/TEXTAREA|INPUT/i.test(el.nodeName)) {
+                el.innerHTML = args.content;
+            } else {
+                el.value = args.content;
+            }
+
+            // save is triggered on debounced keyup
+            if (debounced) {
+                // trigger custom change event
+                ed.onWfEditorChange.dispatch(ed, args);
+            }
+
+            return args.content;
         },
 
         toggle: function () {
             var ed = this.editor;
-            var element = ed.getElement(),
-                container = element.parentNode;
+            var self = this,
+                s = ed.settings;
+
+            var element = ed.getElement();
+            var container = element.parentNode;
 
             // get tabs header
             var header = DOM.getPrev(element, '.wf-editor-header');
 
+            var div = DOM.get(ed.id + '_editor_source');
+            var textarea = DOM.get(ed.id + '_editor_source_textarea');
+
             // get editor iframe height
-            var ifrHeight = parseInt(DOM.get(ed.id + '_ifr').style.height, 10) || ed.settings.height;
+            var ifrHeight = parseInt(DOM.get(ed.id + '_ifr').style.height, 10) || s.height;
 
             var o = tinymce.util.Storage.getHash("TinyMCE_" + ed.id + "_size");
 
@@ -189,34 +196,149 @@
                 ifrHeight = o.height;
             }
 
-            if (DOM.isHidden(element)) {
-                DOM.show(element);
+            // get content from textarea / div
+            var content = tinymce.is(element.value) ? element.value : element.innerHTML;
 
-                DOM.removeClass(container, 'mce-loading');
+            // remove system stuff
+            content = cleanContent(ed, content);
 
-                // dispatch on show to trigger event - SPPageBuilder
-                ed.onChange.dispatch();
+            if (!div) {
+                // create the container
+                div = DOM.add(container, 'div', {
+                    role: 'textbox',
+                    id: ed.id + '_editor_source',
+                    'class': 'wf-editor-source'
+                });
 
+                var skin = s.skin_class || 'defaultSkin';
+
+                DOM.addClass(div, skin);
+
+                textarea = DOM.create('textarea', {
+                    'id': ed.id + '_editor_source_textarea'
+                });
+
+                DOM.add(div, textarea);
+
+                // set content
+                textarea.value = content;
+
+                // create statusbar
+                var statusbar = DOM.add(div, 'div', {
+                    'id': ed.id + '_editor_source_statusbar',
+                    'class': 'mceStatusbar mceLast'
+                }, '<div class="mcePathRow"></div><div tabindex="-1" class="mceResize" id="' + ed.id + '_editor_source_resize"><span class="mceIcon mce_resize"></span></div>');
+
+                var resize = DOM.get(ed.id + '_editor_source_resize');
+
+                // cancel default click
+                Event.add(resize, 'click', function (e) {
+                    e.preventDefault();
+                });
+
+                // Resize source editor
+                Event.add(resize, 'mousedown', function (e) {
+                    var mm1, mu1, sx, sy, sw, sh, w, h;
+
+                    e.preventDefault();
+
+                    function resizeTo(w, h) {
+                        w = Math.max(w, 300);
+                        h = Math.max(h, 200);
+
+                        textarea.style.height = h + 'px';
+                        container.style.maxWidth = w + 'px';
+
+                        // pass through values
+                        ed.settings.container_width = w;
+                        ed.settings.container_height = h + statusbar.offsetHeight;
+
+                        // remove interface height if set                        
+                        h = h - (ed.settings.interface_height || 0);
+
+                        // resize editor
+                        ed.theme.resizeTo(w, h);
+                    }
+
+                    function resizeOnMove(e) {
+                        e.preventDefault();
+
+                        w = sw + (e.screenX - sx);
+                        h = sh + (e.screenY - sy);
+
+                        resizeTo(w, h);
+
+                        DOM.addClass(resize, 'wf-editor-source-resizing');
+                    }
+
+                    function endResize(e) {
+                        e.preventDefault();
+
+                        // Stop listening
+                        Event.remove(DOM.doc, 'mousemove', mm1);
+                        Event.remove(DOM.doc, 'mouseup', mu1);
+
+                        w = sw + (e.screenX - sx);
+                        h = sh + (e.screenY - sy);
+
+                        resizeTo(w, h);
+
+                        DOM.removeClass(resize, 'wf-editor-source-resizing');
+                    }
+
+                    if (DOM.hasClass(resize, 'wf-editor-source-resizing')) {
+                        endResize(e);
+                        return false;
+                    }
+
+                    // Get the current rect size
+                    sx = e.screenX;
+                    sy = e.screenY;
+
+                    sw = w = container.offsetWidth;
+                    sh = h = textarea.clientHeight;
+
+                    // Register envent handlers
+                    mm1 = Event.add(DOM.doc, 'mousemove', resizeOnMove);
+                    mu1 = Event.add(DOM.doc, 'mouseup', endResize);
+                });
+
+                // ugly hack to detect WidgetKit and SpPageBuilder extensions
+                if (hasCustomEditorInit(ed)) {
+                    // Debounce keyup function
+                    var keyup = debounce(function (e) {
+
+                        var value = textarea.value;
+
+                        // trim value
+                        value = value.replace(/^\s*|\s*$/g, '');
+
+                        // pass to the textarea
+                        self.save(value, true);
+                    }, 300);
+
+                    DOM.bind(textarea, 'input blur', function () {
+                        keyup();
+                    });
+                }
             } else {
-                DOM.hide(element);
+                DOM.show(div);
+
+                // set content
+                textarea.value = content;
             }
 
+            DOM.removeClass(container, 'mce-loading');
+
             // get height from setting or session data or editor textarea
-            var height = ed.settings.container_height || sessionStorage.getItem('wf-editor-container-height') || ifrHeight;
+            var height = ed.settings.container_height || sessionStorage.getItem('wf-editor-container-height') || (ifrHeight + statusbar.offsetHeight);
 
             if (DOM.hasClass(container, 'mce-fullscreen')) {
                 var vp = DOM.getViewPort();
-                height = vp.h - header.offsetHeight;
+                height = vp.h - header.offsetHeight - statusbar.offsetHeight - 4;
             }
 
-            DOM.setStyle(element, 'height', height);
-
-            // get width from setting or session data or editor textarea
-            var width = ed.settings.container_width || sessionStorage.getItem('wf-editor-container-width');
-
-            if (!DOM.hasClass(container, 'mce-fullscreen')) {
-                DOM.setStyle(element, 'width', width);
-            }
+            DOM.setStyles(textarea, { height : height });
         },
 
         getSelection: function () {
