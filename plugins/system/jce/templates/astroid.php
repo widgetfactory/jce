7f735f24d30a670ebf2505e96ab8ce37b35e6a46
Ryan <ryan@widgetfactorylimited.co.uk>
2022-10-11 20:09:01 +0100

    Update to include Joomla 4 paths

--- plugins/system/jce/templates/astroid.php
+++ plugins/system/jce/templates/astroid.php
@@ -10,17 +10,29 @@ class WfTemplateAstroid extends JPlugin
 {
     public function onWfGetTemplateStylesheets(&$files, $template)
     {                        
+        // Joomla 4
+        $path = JPATH_SITE . '/media/templates/site/' . $template->name;
+            
+        if (is_dir($path . '/astroid')) {
+            $items = glob($path . '/css/compiled-*.css');
+
+            foreach($items as $item) {
+                $files[] = 'media/templates/site/' . $template->name . '/css/' . basename($item);
+            }
+
+            return true;
+        }
+        
+        // Joomla 3
         $path = JPATH_SITE . '/templates/' . $template->name;
 
-        if (!is_dir($path . '/astroid')) {
-            return false;
-        }
-            
-        $items = glob($path . '/css/compiled-*.css');
+        if (is_dir($path . '/astroid')) {
+            $items = glob($path . '/css/compiled-*.css');
 
-        foreach($items as $item) {
-            // add compiled css file
-            $files[] = 'templates/' . $template->name . '/css/' . basename($item);
+            foreach($items as $item) {
+                // add compiled css file
+                $files[] = 'templates/' . $template->name . '/css/' . basename($item);
+            }
         }
     }
 }
