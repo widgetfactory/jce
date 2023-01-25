f7042a9d4ae38a127b3800e94c115f58628b50bc
Ryan <ryan@widgetfactorylimited.co.uk>
2023-01-25 11:33:43 +0000

    Update tiny_mce.js

--- components/com_jce/editor/tiny_mce/tiny_mce.js
+++ components/com_jce/editor/tiny_mce/tiny_mce.js
@@ -23039,7 +23039,7 @@
         } else {
           DOM.setAttrib(item, 'role', 'option');
 
-          if (s.icon || s.icon_src) {
+          if ((s.icon || s.icon_src) && (!s.svg && !s.image)) {
             icon = DOM.add(item, 'span', {
               'class': 'mceIcon' + (s.icon ? ' mce_' + s.icon : '')
             });
@@ -23052,7 +23052,7 @@
 
             DOM.addClass(item, 'mceHasIcon');
           }
-
+          
           if (s.image) {
             DOM.add(item, 'span', {
               'class': 'mceImage',
