f2e3478d37a42fbd57805fbfab0269d75589cb90
Ryan <ryan@widgetfactorylimited.co.uk>
2023-02-17 15:26:33 +0000

    Skip internal .mce- class names

--- components/com_jce/editor/tiny_mce/plugins/importcss/editor_plugin.js
+++ components/com_jce/editor/tiny_mce/plugins/importcss/editor_plugin.js
@@ -421,7 +421,7 @@
                 each(r.selectorText.split(','), function (v) {                  
                   v = v.trim();
 
-                  if (v.indexOf('.mce') == 0) {
+                  if (v.indexOf('.mce') == 0 || v.indexOf('.mce-') !== -1) {
                     return;
                   }
 
