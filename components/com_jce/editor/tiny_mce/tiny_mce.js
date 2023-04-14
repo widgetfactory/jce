755008c29e3602d133047fe96b71dc0ca43de03e
Ryan <ryan@widgetfactorylimited.co.uk>
2023-04-14 11:20:01 +0100

    Update tiny_mce.js

--- components/com_jce/editor/tiny_mce/tiny_mce.js
+++ components/com_jce/editor/tiny_mce/tiny_mce.js
@@ -13108,6 +13108,87 @@
   })(tinymce);
 
   /**
+   * MousePosition.js
+   *
+   * Released under LGPL License.
+   * Copyright (c) 1999-2017 Ephox Corp. All rights reserved
+   *
+   * License: http://www.tinymce.com/license
+   * Contributing: http://www.tinymce.com/contributing
+   */
+
+  /**
+   * This module calculates an absolute coordinate inside the editor body for both local and global mouse events.
+   *
+   * @private
+   * @class tinymce.dom.MousePosition
+   */
+  (function (tinymce) {
+      var getAbsolutePosition = function (elm) {
+        var doc, docElem, win, clientRect;
+
+        clientRect = elm.getBoundingClientRect();
+        doc = elm.ownerDocument;
+        docElem = doc.documentElement;
+        win = doc.defaultView;
+
+        return {
+          top: clientRect.top + win.pageYOffset - docElem.clientTop,
+          left: clientRect.left + win.pageXOffset - docElem.clientLeft
+        };
+      };
+
+      var getBodyPosition = function (editor) {
+        return editor.inline ? getAbsolutePosition(editor.getBody()) : { left: 0, top: 0 };
+      };
+
+      var getScrollPosition = function (editor) {
+        var body = editor.getBody();
+        return editor.inline ? { left: body.scrollLeft, top: body.scrollTop } : { left: 0, top: 0 };
+      };
+
+      var getBodyScroll = function (editor) {
+        var body = editor.getBody(), docElm = editor.getDoc().documentElement;
+        var inlineScroll = { left: body.scrollLeft, top: body.scrollTop };
+        var iframeScroll = { left: body.scrollLeft || docElm.scrollLeft, top: body.scrollTop || docElm.scrollTop };
+
+        return editor.inline ? inlineScroll : iframeScroll;
+      };
+
+      var getMousePosition = function (editor, event) {
+        if (event.target.ownerDocument !== editor.getDoc()) {
+          var iframePosition = getAbsolutePosition(editor.getContentAreaContainer());
+          var scrollPosition = getBodyScroll(editor);
+
+          return {
+            left: event.pageX - iframePosition.left + scrollPosition.left,
+            top: event.pageY - iframePosition.top + scrollPosition.top
+          };
+        }
+
+        return {
+          left: event.pageX,
+          top: event.pageY
+        };
+      };
+
+      var calculatePosition = function (bodyPosition, scrollPosition, mousePosition) {
+        return {
+          pageX: (mousePosition.left - bodyPosition.left) + scrollPosition.left,
+          pageY: (mousePosition.top - bodyPosition.top) + scrollPosition.top
+        };
+      };
+
+      var calc = function (editor, event) {
+        return calculatePosition(getBodyPosition(editor), getScrollPosition(editor), getMousePosition(editor, event));
+      };
+
+      return {
+        calc: calc
+      };
+  })(tinymce);
+
+  /**
    * ExtendingChar.js
    *
    * Released under LGPL License.
@@ -28716,6 +28797,7 @@
         self.editorCommands = new tinymce.EditorCommands(self);
 
         self._selectionOverrides = new tinymce.SelectionOverrides(self);
+        self._selectionOverrides = new tinymce.SelectionOverrides(self);
 
         self.onExecCommand.add(function (editor, command) {
           // Don't refresh the select lists until caret move
@@ -38327,6 +38409,297 @@
   })(tinymce);
 
   /**
+   * DragDropOverrides.js
+   *
+   * Released under LGPL License.
+   * Copyright (c) 1999-2017 Ephox Corp. All rights reserved
+   *
+   * License: http://www.tinymce.com/license
+   * Contributing: http://www.tinymce.com/contributing
+   */
+
+  /**
+   * This module contains logic overriding the drag/drop logic of the editor.
+   *
+   * @private
+   * @class tinymce.DragDropOverrides
+   */
+  (function (tinymce) {
+    var NodeType = tinymce.dom.NodeType; tinymce.dom.DOMUtils; var Delay = tinymce.util.Delay, Arr = tinymce.util.Arr, MousePosition = tinymce.dom.MousePosition, Fun = tinymce.util.Fun;
+
+    var isContentEditableFalse = NodeType.isContentEditableFalse,
+      isContentEditableTrue = NodeType.isContentEditableTrue;
+
+    var isDraggable = function (rootElm, elm) {
+      return isContentEditableFalse(elm) && elm !== rootElm;
+    };
+
+    var isValidDropTarget = function (editor, targetElement, dragElement) {
+      if (targetElement === dragElement || editor.dom.isChildOf(targetElement, dragElement)) {
+        return false;
+      }
+
+      if (isContentEditableFalse(targetElement)) {
+        return false;
+      }
+
+      return true;
+    };
+
+    var cloneElement = function (elm) {
+      var cloneElm = elm.cloneNode(true);
+      cloneElm.removeAttribute('data-mce-selected');
+      return cloneElm;
+    };
+
+    var createGhost = function (editor, elm, width, height) {
+      var clonedElm = elm.cloneNode(true);
+
+      editor.dom.setStyles(clonedElm, { width: width, height: height });
+      editor.dom.setAttrib(clonedElm, 'data-mce-selected', null);
+
+      var ghostElm = editor.dom.create('div', {
+        'class': 'mce-drag-container',
+        'data-mce-bogus': 'all',
+        unselectable: 'on',
+        contenteditable: 'false'
+      });
+
+      editor.dom.setStyles(ghostElm, {
+        position: 'absolute',
+        opacity: 0.5,
+        overflow: 'hidden',
+        border: 0,
+        padding: 0,
+        margin: 0,
+        width: width,
+        height: height
+      });
+
+      editor.dom.setStyles(clonedElm, {
+        margin: 0,
+        boxSizing: 'border-box'
+      });
+
+      ghostElm.appendChild(clonedElm);
+
+      return ghostElm;
+    };
+
+    var appendGhostToBody = function (ghostElm, bodyElm) {
+      if (ghostElm.parentNode !== bodyElm) {
+        bodyElm.appendChild(ghostElm);
+      }
+    };
+
+    var moveGhost = function (ghostElm, position, width, height, maxX, maxY) {
+      var overflowX = 0, overflowY = 0;
+
+      ghostElm.style.left = position.pageX + 'px';
+      ghostElm.style.top = position.pageY + 'px';
+
+      if (position.pageX + width > maxX) {
+        overflowX = (position.pageX + width) - maxX;
+      }
+
+      if (position.pageY + height > maxY) {
+        overflowY = (position.pageY + height) - maxY;
+      }
+
+      ghostElm.style.width = (width - overflowX) + 'px';
+      ghostElm.style.height = (height - overflowY) + 'px';
+    };
+
+    var removeElement = function (elm) {
+      if (elm && elm.parentNode) {
+        elm.parentNode.removeChild(elm);
+      }
+    };
+
+    var isLeftMouseButtonPressed = function (e) {
+      return e.button === 0;
+    };
+
+    var hasDraggableElement = function (state) {
+      return state.element;
+    };
+
+    var applyRelPos = function (state, position) {
+      return {
+        pageX: position.pageX - state.relX,
+        pageY: position.pageY + 5
+      };
+    };
+
+    var start = function (state, editor) {
+      return function (e) {
+        if (isLeftMouseButtonPressed(e)) {
+          var ceElm = Arr.find(editor.dom.getParents(e.target), Fun.or(isContentEditableFalse, isContentEditableTrue));
+
+          if (isDraggable(editor.getBody(), ceElm)) {
+            var elmPos = editor.dom.getPos(ceElm);
+            var bodyElm = editor.getBody();
+            var docElm = editor.getDoc().documentElement;
+
+            state.element = ceElm;
+            state.screenX = e.screenX;
+            state.screenY = e.screenY;
+            state.maxX = (editor.inline ? bodyElm.scrollWidth : docElm.offsetWidth) - 2;
+            state.maxY = (editor.inline ? bodyElm.scrollHeight : docElm.offsetHeight) - 2;
+            state.relX = e.pageX - elmPos.x;
+            state.relY = e.pageY - elmPos.y;
+            state.width = ceElm.offsetWidth;
+            state.height = ceElm.offsetHeight;
+            state.ghost = createGhost(editor, ceElm, state.width, state.height);
+          }
+        }
+      };
+    };
+
+    var move = function (state, editor) {
+      // Reduces laggy drag behavior on Gecko
+      var throttledPlaceCaretAt = Delay.throttle(function (clientX, clientY) {
+        editor._selectionOverrides.hideFakeCaret();
+        editor.selection.placeCaretAt(clientX, clientY);
+      }, 0);
+
+      return function (e) {
+        var movement = Math.max(Math.abs(e.screenX - state.screenX), Math.abs(e.screenY - state.screenY));
+
+        if (hasDraggableElement(state) && !state.dragging && movement > 10) {
+          //var args = editor.fire('dragstart', { target: state.element });
+
+          var args = {
+            target: state.element
+          };
+
+          editor.dom.fire(editor.getBody(), 'dragstart', args);
+
+          if (args.isDefaultPrevented()) {
+            return;
+          }
+
+          state.dragging = true;
+          editor.focus();
+        }
+
+        if (state.dragging) {
+          var targetPos = applyRelPos(state, MousePosition.calc(editor, e));
+
+          appendGhostToBody(state.ghost, editor.getBody());
+          moveGhost(state.ghost, targetPos, state.width, state.height, state.maxX, state.maxY);
+
+          throttledPlaceCaretAt(e.clientX, e.clientY);
+        }
+      };
+    };
+
+    // Returns the raw element instead of the fake cE=false element
+    var getRawTarget = function (selection) {
+      var rng = selection.getSel().getRangeAt(0);
+      var startContainer = rng.startContainer;
+      return startContainer.nodeType === 3 ? startContainer.parentNode : startContainer;
+    };
+
+    var drop = function (state, editor) {
+      return function (e) {
+        if (state.dragging) {
+          if (isValidDropTarget(editor, getRawTarget(editor.selection), state.element)) {
+            var targetClone = cloneElement(state.element);
+
+            /*var args = editor.fire('drop', {
+              targetClone: targetClone,
+              clientX: e.clientX,
+              clientY: e.clientY
+            });*/
+
+            var args = {
+              targetClone: targetClone,
+              clientX: e.clientX,
+              clientY: e.clientY
+            };
+
+            editor.dom.fire(editor.getBody(), 'drop', args);
+
+            if (!args.isDefaultPrevented()) {
+              targetClone = args.targetClone;
+
+              /*editor.undoManager.transact(function () {
+                removeElement(state.element);
+                editor.insertContent(editor.dom.getOuterHTML(targetClone));
+                editor._selectionOverrides.hideFakeCaret();
+              });*/
+
+              editor.undoManager.add();
+
+              removeElement(state.element);
+              editor.insertContent(editor.dom.getOuterHTML(targetClone));
+              editor._selectionOverrides.hideFakeCaret();
+            }
+          }
+        }
+
+        removeDragState(state);
+      };
+    };
+
+    var removeDragState = function (state) {
+      state.dragging = false;
+      state.element = null;
+      removeElement(state.ghost);
+    };
+
+    var bindFakeDragEvents = function (editor) {
+      var state = {}, dragStartHandler, dragHandler, dropHandler;
+      dragStartHandler = start(state, editor);
+      dragHandler = move(state, editor);
+      dropHandler = drop(state, editor);
+
+      //editor.on('mousedown', dragStartHandler);
+      //editor.on('mousemove', dragHandler);
+      //editor.on('mouseup', dropHandler);
+
+      editor.dom.bind(editor.getBody(), 'mousedown', dragStartHandler);
+      editor.dom.bind(editor.getBody(), 'mousemove', dragHandler);
+      editor.dom.bind(editor.getBody(), 'mouseup', dropHandler);
+
+      //pageDom.bind(rootDocument, 'mousemove', dragHandler);
+      //pageDom.bind(rootDocument, 'mouseup', dragEndHandler);
+
+      /*editor.on('remove', function () {
+        pageDom.unbind(rootDocument, 'mousemove', dragHandler);
+        pageDom.unbind(rootDocument, 'mouseup', dragEndHandler);
+      });*/
+
+      /*editor.onRemove.add(function () {
+        pageDom.unbind(rootDocument, 'mousemove', dragHandler);
+        pageDom.unbind(rootDocument, 'mouseup', dragEndHandler);
+      });*/
+    };
+
+    var blockIeDrop = function (editor) {
+      editor.dom.bind(editor.getBody(), 'drop', function (e) {
+        // FF doesn't pass out clientX/clientY for drop since this is for IE we just use null instead
+        var realTarget = typeof e.clientX !== 'undefined' ? editor.getDoc().elementFromPoint(e.clientX, e.clientY) : null;
+
+        if (isContentEditableFalse(realTarget) || isContentEditableFalse(editor.dom.getContentEditableParent(realTarget))) {
+          e.preventDefault();
+        }
+      });
+    };
+
+    var init = function (editor) {
+      bindFakeDragEvents(editor);
+      blockIeDrop(editor);
+    };
+
+    tinymce.DragDropOverrides = {
+      init: init
+    };
+
+  })(tinymce);
+
+  /**
    * SelectionOverrides.js
    *
    * Released under LGPL License.
@@ -38357,7 +38730,7 @@
     var NodeType = tinymce.dom.NodeType, RangeUtils = tinymce.dom.RangeUtils;
     var VK = tinymce.VK, Fun = tinymce.util.Fun, Arr = tinymce.util.Arr;
 
-    var Dispatcher = tinymce.util.Dispatcher;
+    var Dispatcher = tinymce.util.Dispatcher; tinymce.DragDropOverrides;
 
     var curry = Fun.curry,
       isContentEditableTrue = NodeType.isContentEditableTrue,
@@ -39211,6 +39584,8 @@
             }
           }
         });
+
+        //DragDropOverrides.init(editor);
       }
 
       function isWithinCaretContainer(node) {
