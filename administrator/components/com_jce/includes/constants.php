c1a52d0ae414eddd534deee9f6d6cb74bc6c8ba3
Ryan <ryan@widgetfactorylimited.co.uk>
2023-09-19 09:34:59 +0100

    Update constants.php

--- administrator/components/com_jce/includes/constants.php
+++ administrator/components/com_jce/includes/constants.php
@@ -1,14 +1,14 @@
 <?php
 
 /**
- * @copyright 	Copyright (c) 2009-2022 Ryan Demmer. All rights reserved
- * @license   	GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * JCE is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses
+ * @package     JCE
+ * @subpackage  Admin
+ *
+ * @copyright   Copyright (C) 2005 - 2023 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (c) 2009-2023 Ryan Demmer. All rights reserved
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
-defined('JPATH_PLATFORM') or die('RESTRICTED');
+defined('JPATH_PLATFORM') or die;
 
 use Joomla\CMS\Uri\Uri;
 
@@ -16,33 +16,34 @@ use Joomla\CMS\Uri\Uri;
 define('WF_VERSION', '@@version@@');
 
 // JCE Administration Component
-define('WF_ADMINISTRATOR',     JPATH_ADMINISTRATOR.'/components/com_jce');
+define('WF_ADMINISTRATOR', JPATH_ADMINISTRATOR . '/components/com_jce');
 // JCE Site Component
-define('WF_SITE',              JPATH_SITE.'/components/com_jce');
+define('WF_SITE', JPATH_SITE . '/components/com_jce');
 // JCE Plugin
 if (defined('JPATH_PLATFORM')) {
-    define('WF_PLUGIN',        JPATH_SITE.'/plugins/editors/jce');
+    define('WF_PLUGIN', JPATH_SITE . '/plugins/editors/jce');
 } else {
-    define('WF_PLUGIN',        JPATH_SITE.'/plugins/editors');
+    define('WF_PLUGIN', JPATH_SITE . '/plugins/editors');
 }
 // JCE Editor
-define('WF_EDITOR',            WF_SITE.'/editor');
+define('WF_EDITOR', WF_SITE . '/editor');
 // JCE Editor Plugins
-define('WF_EDITOR_PLUGINS',    WF_EDITOR.'/tiny_mce/plugins');
+define('WF_EDITOR_PLUGINS', WF_EDITOR . '/tiny_mce/plugins');
 // JCE Editor Themes
-define('WF_EDITOR_THEMES',     WF_EDITOR.'/tiny_mce/themes');
+define('WF_EDITOR_THEMES', WF_EDITOR . '/tiny_mce/themes');
 // JCE Editor Libraries
-define('WF_EDITOR_LIBRARIES',  WF_EDITOR.'/libraries');
+define('WF_EDITOR_LIBRARIES', WF_EDITOR . '/libraries');
 // JCE Editor Classes
-define('WF_EDITOR_CLASSES',    WF_EDITOR_LIBRARIES.'/classes');
+define('WF_EDITOR_CLASSES', WF_EDITOR_LIBRARIES . '/classes');
 // JCE Editor Extensions
-define('WF_EDITOR_EXTENSIONS', WF_EDITOR.'/extensions');
+define('WF_EDITOR_EXTENSIONS', WF_EDITOR . '/extensions');
 
 define('WF_EDITOR_URI', Uri::root(true) . '/components/com_jce/editor');
 
 define('WF_EDITOR_PRO', '@@is_pro@@');
 
-// required for some plugins
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-}
+// required for some legacy plugins
+defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+
+// legacy plugin support
+define('_WF_EXT', 1);
