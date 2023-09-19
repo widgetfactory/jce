a3030ab55fa836b4937fb7c3be9009f37e36098d
Ryan <ryan@widgetfactorylimited.co.uk>
2023-09-19 09:26:25 +0100

    Update header and add onWfMediaFieldGetOptions event

--- administrator/components/com_jce/helpers/browser.php
+++ administrator/components/com_jce/helpers/browser.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * @copyright     Copyright (c) 2009-2023 Ryan Demmer. All rights reserved
- * @license       GNU/GPL 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
- * JCE is free software. This version may have been modified pursuant
- * to the GNU General Public License, and as distributed it includes or
- * is derivative of works licensed under the GNU General Public License or
- * other free or open source software licenses
+ * @package     JCE
+ * @subpackage  Admin
+ *
+ * @copyright   Copyright (C) 2005 - 2023 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (c) 2009-2023 Ryan Demmer. All rights reserved
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 defined('JPATH_PLATFORM') or die;
 
@@ -116,6 +116,8 @@ abstract class WfBrowserHelper
             $data['accept'] = implode(',', array_filter($data['accept']));
 
             $data['upload'] = (bool) $wf->getParam('browser.mediafield_upload', 1);
+
+            $app->triggerEvent('onWfMediaFieldGetOptions', array(&$data));
         }
 
         return $data;
