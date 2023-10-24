5bd6c7200286a4ee1ee5472d3a70027317cd79de
Ryan <ryan@widgetfactorylimited.co.uk>
2023-10-24 10:26:18 +0100

    Add banner

--- media/com_jce/editor/tinymce/rollup.config.js
+++ media/com_jce/editor/tinymce/rollup.config.js
@@ -1,7 +1,8 @@
 /* eslint-disable */
 
 import replace from '@rollup/plugin-replace';
-import {eslint} from 'rollup-plugin-eslint';
+import { eslint } from 'rollup-plugin-eslint';
+/*import { uglify } from "rollup-plugin-uglify";*/
 
 export default [
   {
@@ -15,11 +16,11 @@ export default [
       globals: {
         wfe: 'wfe'
       },
-      banner: '/* eslint-disable */'
+      banner: '/* eslint-disable */\n/* \n * This file includes original and modified code from various versions of Tinymce. \n * \n * Source: https://github.com/widgetfactory/tinymce-muon \n * Copyright (c) Tiny Technologies, Inc. All rights reserved. \n * Copyright (c) 1999-2015 Ephox Corp. All rights reserved. \n * Copyright, Moxiecode Systems AB. All rights reserved. \n * Copyright (c) 2009 - 2023 Ryan Demmer. All rights reserved. \n * For a detailed history of modifications, refer to the Git commit history. \n * Licensed under the GNU/LGPL 2.1 or later: http://www.gnu.org/licenses/old-licenses/lgpl-2.1.html \n */'
     },
 
     plugins: [
-      //uglify({mangle: false})
+      //uglify({ mangle: false }),
       replace({
         'tinymce$1': 'tinymce'
       }),
@@ -27,6 +28,4 @@ export default [
       })
     ]
   }
-
-
 ];
