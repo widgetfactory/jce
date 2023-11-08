c9f9634aca22bfe5b698ff0ba18821a26c6b19bc
Ryan <ryan@widgetfactorylimited.co.uk>
2023-11-08 16:58:01 +0000

    Pass through $buttons state to getButtons method (WidgetKit bug)

--- plugins/editors/jce/jce.php
+++ plugins/editors/jce/jce.php
@@ -233,7 +233,7 @@ class plgEditorJCE extends CMSPlugin
         }
 
         // easiest way to get buttons across versions
-        $buttons = Editor::getInstance('jce')->getButtons($name);
+        $buttons = Editor::getInstance('jce')->getButtons($name, $buttons);
 
         if (!empty($buttons)) {
             foreach ($buttons as $i => $button) {
@@ -272,7 +272,7 @@ class plgEditorJCE extends CMSPlugin
     private function displayButtons($name, $buttons, $asset, $author)
     {
         // easiest way to get buttons across versions
-        $buttons = Editor::getInstance('jce')->getButtons($name);
+        $buttons = Editor::getInstance('jce')->getButtons($name, $buttons);
 
         if (!empty($buttons)) {
             // fix some legacy buttons
