3014a38aa53d3b463df743917419313981f3964c
Ryan <ryan@widgetfactorylimited.co.uk>
2024-01-16 09:45:13 +0000

    Update copyright year

--- administrator/components/com_jce/controller/profile.php
+++ administrator/components/com_jce/controller/profile.php
@@ -5,7 +5,7 @@
  * @subpackage  Admin
  *
  * @copyright   Copyright (C) 2005 - 2023 Open Source Matters, Inc. All rights reserved.
- * @copyright   Copyright (c) 2009-2023 Ryan Demmer. All rights reserved
+ * @copyright   Copyright (c) 2009-2024 Ryan Demmer. All rights reserved
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
