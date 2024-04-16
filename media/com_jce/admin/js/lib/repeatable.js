cfc65f36a3c02e8c067b7a35770a7fb94019a6c2
Ryan <ryan@widgetfactorylimited.co.uk>
2024-04-16 11:26:49 +0100

    Fix repeatable Media Field when used in a Profile

--- media/com_jce/admin/js/lib/repeatable.js
+++ media/com_jce/admin/js/lib/repeatable.js
@@ -1,9 +1,45 @@
-/* global jQuery */
+/* global jQuery, Joomla */
 /* eslint-disable consistent-this */
 (function ($) {
     $(document).ready(function () {
+        function isImage(value) {
+            return value && /\.(jpg|jpeg|png|gif|svg|apng|webp)$/.test(value);
+        }
+        
+        function createElementMedia(elm) {
+            var modalElement = $('.joomla-modal', elm).get(0);
+    
+            if (modalElement && window.bootstrap && window.bootstrap.Modal) {
+                Joomla.initialiseModal(modalElement, {
+                    isJoomla: true
+                });
+    
+                $('.button-select', elm).on('click', function (e) {
+                    e.preventDefault();
+                    modalElement.open();
+                });
+            }
+    
+            $('.button-clear', elm).on('click', function (e) {
+                e.preventDefault();
+                $('.wf-media-input', elm).val('').trigger('change');
+            });
+    
+            $('.wf-media-input', elm).on('change', function () {
+                var path = Joomla.getOptions('system.paths', {}).root || '';
+    
+                var src = '';
+    
+                if (isImage(this.value)) {
+                    src = path + '/' + this.value;
+                }
+    
+                $('.field-media-preview img', elm).attr('src', src);
+            }).trigger('change');
+        }
+        
         // repeatable
-        $('.controls').on('click', '.form-field-repeatable-add', function (e) {            
+        $('.controls').on('click', '.form-field-repeatable-add', function (e) {
             e.preventDefault();
             e.stopPropagation();
             // get repeatable container, clone item
@@ -44,15 +80,30 @@
             $item.find(':input[name]').val('').trigger('change').removeClass('isdirty');
             $item.find('select[name]').find('option:first').prop('selected', true).parent().trigger('change').removeClass('isdirty');
 
-            // fix media field
-            $item.find('.field-media-wrapper').each(function () {
+            // fix media fields
+            $item.add($repeatable).find('.field-media-wrapper').each(function () {
+                $(this).find('.wf-media-input').removeClass('wf-media-input-converted');
+                
+                // store input value
+                var value = $(this).find('.wf-media-input').val();
+
+                // clear events
+                var html = $(this).html();
+                $(this).html(html);
+
+                // restore input value
+                $(this).find('.wf-media-input').val(value);
+
                 // re-initlialize
                 if ($.fn.fieldMedia) {
                     $(this).fieldMedia();
                 }
 
                 $(document).trigger('subform-row-add', [this]);
+
+                createElementMedia(this);
             });
+
             // reset radio items
             $parent.find('input[type="radio"][checked]').each(function () {
                 this.checked = !!this.getAttribute('checked');
@@ -64,7 +115,7 @@
             }
         });
 
-        $('.controls').on('click', '.form-field-repeatable-remove', function (e) {            
+        $('.controls').on('click', '.form-field-repeatable-remove', function (e) {
             e.preventDefault();
             var $repeatable = $(this).parents('.form-field-repeatable-item'), $parent = $repeatable.parent();
 
