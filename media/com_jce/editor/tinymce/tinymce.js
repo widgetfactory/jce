a90dfcb9fc8f154ae174964afb0f0b9f8d5d8ae6
Ryan <ryan@widgetfactorylimited.co.uk>
2024-07-12 11:15:20 +0100

    Update tinymce.js

--- media/com_jce/editor/tinymce/tinymce.js
+++ media/com_jce/editor/tinymce/tinymce.js
@@ -9180,12 +9180,17 @@
 
         if (attrs) {
           var bool = [];
-          
+
           for (i = 0, l = attrs.length; i < l; i++) {
             attr = attrs[i];
 
             if (attr["boolean"]) {
-              bool.push(' ', attr.name);
+              if (settings.schema == 'html5-strict') {
+                // boolean attributes in HTML5 are written without a value
+                bool.push(' ', attr.name);
+              } else {
+                bool.push(' ', attr.name, '="', encode('' + attr.name, true), '"');
+              }
             } else {
               html.push(' ', attr.name, '="', encode('' + attr.value, true), '"');
             }
@@ -9200,7 +9205,7 @@
           // use void tag
           if (settings.schema == 'html5-strict') {
             html[html.length] = '>';
-          // use self-closing tag
+            // use self-closing tag
           } else {
             html[html.length] = ' />';
           }
@@ -9216,20 +9221,20 @@
       },
 
       /**
-  		 * Writes the a end element such as </p>.
-  		 *
-  		 * @method end
-  		 * @param {String} name Name of the element.
-  		 */
+       * Writes the a end element such as </p>.
+       *
+       * @method end
+       * @param {String} name Name of the element.
+       */
       end: function (name) {
         var value;
 
         /*if (indent && indentBefore[name] && html.length > 0) {
-  				value = html[html.length - 1];
+          value = html[html.length - 1];
 
-  				if (value.length > 0 && value !== '\n')
-  					html.push('\n');
-  			}*/
+          if (value.length > 0 && value !== '\n')
+            html.push('\n');
+        }*/
 
         html.push('</', name, '>');
 
@@ -9243,12 +9248,12 @@
       },
 
       /**
-  		 * Writes a text node.
-  		 *
-  		 * @method text
-  		 * @param {String} text String to write out.
-  		 * @param {Boolean} raw Optional raw state if true the contents wont get encoded.
-  		 */
+       * Writes a text node.
+       *
+       * @method text
+       * @param {String} text String to write out.
+       * @param {Boolean} raw Optional raw state if true the contents wont get encoded.
+       */
       text: function (text, raw) {
         if (text.length > 0) {
           html[html.length] = raw ? text : encode(text);
@@ -9256,32 +9261,32 @@
       },
 
       /**
-  		 * Writes a cdata node such as <![CDATA[data]]>.
-  		 *
-  		 * @method cdata
-  		 * @param {String} text String to write out inside the cdata.
-  		 */
+       * Writes a cdata node such as <![CDATA[data]]>.
+       *
+       * @method cdata
+       * @param {String} text String to write out inside the cdata.
+       */
       cdata: function (text) {
         html.push('<![CDATA[', text, ']]>');
       },
 
       /**
-  		 * Writes a comment node such as <!-- Comment -->.
-  		 *
-  		 * @method cdata
-  		 * @param {String} text String to write out inside the comment.
-  		 */
+       * Writes a comment node such as <!-- Comment -->.
+       *
+       * @method cdata
+       * @param {String} text String to write out inside the comment.
+       */
       comment: function (text) {
         html.push('<!--', text, '-->');
       },
 
       /**
-  		 * Writes a PI node such as <?xml attr="value" ?>.
-  		 *
-  		 * @method pi
-  		 * @param {String} name Name of the pi.
-  		 * @param {String} text String to write out inside the pi.
-  		 */
+       * Writes a PI node such as <?xml attr="value" ?>.
+       *
+       * @method pi
+       * @param {String} name Name of the pi.
+       * @param {String} text String to write out inside the pi.
+       */
       pi: function (name, text) {
         if (text) {
           html.push('<?', name, ' ', encode(text), '?>');
@@ -9295,30 +9300,30 @@
       },
 
       /**
-  		 * Writes a doctype node such as <!DOCTYPE data>.
-  		 *
-  		 * @method doctype
-  		 * @param {String} text String to write out inside the doctype.
-  		 */
+       * Writes a doctype node such as <!DOCTYPE data>.
+       *
+       * @method doctype
+       * @param {String} text String to write out inside the doctype.
+       */
       doctype: function (text) {
         html.push('<!DOCTYPE', text, '>', indent ? '\n' : '');
       },
 
       /**
-  		 * Resets the internal buffer if one wants to reuse the writer.
-  		 *
-  		 * @method reset
-  		 */
+       * Resets the internal buffer if one wants to reuse the writer.
+       *
+       * @method reset
+       */
       reset: function () {
         html.length = 0;
       },
 
       /**
-  		 * Returns the contents that got serialized.
-  		 *
-  		 * @method getContent
-  		 * @return {String} HTML contents that got written down.
-  		 */
+       * Returns the contents that got serialized.
+       *
+       * @method getContent
+       * @return {String} HTML contents that got written down.
+       */
       getContent: function () {
         return html.join('').replace(/\n$/, '');
       }
