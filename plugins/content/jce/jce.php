d3d6a8a9345d724e1abcfe48bfc9747cbf3830c8
Ryan <ryan@widgetfactorylimited.co.uk>
2024-07-25 15:56:31 +0100

    Add function comment

--- plugins/content/jce/jce.php
+++ plugins/content/jce/jce.php
@@ -18,6 +18,17 @@ use Joomla\CMS\Form\Form;
  */
 class PlgContentJce extends CMSPlugin
 {
+    
+    /**
+     * Process form fields in content.
+     * This is included to process Joomla Media Fields in 3rd party extensions that call onContentPrepareForm after the System - JCE plugin has been dispatched.
+     *
+     * @param Form $form The form to be altered
+     * @param mixed $data The associated data for the form
+     *
+     * @return bool
+     *
+     */
     public function onContentPrepareForm(Form $form, $data = [])
     {
         Factory::getApplication()->triggerEvent('onWfContentPrepareForm', array($form, $data));
