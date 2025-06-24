3e8c2e6d90d51a0cd9d7eba928c327f3e9070a2e
Ryan <ryan@widgetfactorylimited.co.uk>
2025-06-24 15:57:06 +0100

    Restore manager.php

---
+++ components/com_jce/editor/libraries/classes/manager.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @package     JCE
+ * @subpackage  Editor
+ *
+ * @copyright   Copyright (C) 2005 - 2020 Open Source Matters, Inc. All rights reserved.
+ * @copyright   Copyright (c) 2009-2024 Ryan Demmer. All rights reserved
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+class WFMediaManager extends WFMediaManagerBase
+{
+}
