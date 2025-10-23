e986369924da89f6db747c590a99995e39ad8c5d
Ryan <ryan@widgetfactorylimited.co.uk>
2025-10-23 14:03:23 +0100

    Use the latest compiled css file and load template.css

--- plugins/system/jce/templates/astroid.php
+++ plugins/system/jce/templates/astroid.php
@@ -11,14 +11,27 @@ use Joomla\CMS\Plugin\CMSPlugin;
 class WfTemplateAstroid extends CMSPlugin
 {
     public function onWfGetTemplateStylesheets(&$files, $template)
-    {                        
+    {
         // Joomla 4
         $path = JPATH_SITE . '/media/templates/site/' . $template->name;
-            
+
         if (is_dir($path . '/astroid')) {
             $items = glob($path . '/css/compiled-*.css');
 
-            foreach($items as $item) {
+            // add template css file (bootstrap)
+            if (is_file($path . '/css/template.css')) {
+                $files[] = 'media/templates/site/' . $template->name . '/css/template.css';
+            }
+
+            if (!empty($items)) {
+                // order by modified time from newest to oldest
+                usort($items, function ($a, $b) {
+                    return filemtime($b) - filemtime($a);
+                });
+
+                // get the first item
+                $item = array_shift($items);
+
                 $files[] = 'media/templates/site/' . $template->name . '/css/' . basename($item);
             }
 
@@ -29,18 +42,30 @@ class WfTemplateAstroid extends CMSPlugin
 
             return true;
         }
-        
+
         // Joomla 3
         $path = JPATH_SITE . '/templates/' . $template->name;
 
         if (is_dir($path . '/astroid')) {
             $items = glob($path . '/css/compiled-*.css');
 
-            foreach($items as $item) {
-                // add compiled css file
-                $files[] = 'templates/' . $template->name . '/css/' . basename($item);
+            // add template css file (bootstrap)
+            if (is_file($path . '/css/template.css')) {
+                $files[] = 'templates/' . $template->name . '/css/template.css';
             }
 
+            if (!empty($items)) {
+                // order by modified time from newest to oldest
+                usort($items, function ($a, $b) {
+                    return filemtime($b) - filemtime($a);
+                });
+
+                // get the first item
+                $item = array_shift($items);
+
+                $files[] = 'templates/' . $template->name . '/css/' . basename($item);
+            }
+            
             // add custom css file
             if (is_file($path . '/css/custom.css')) {
                 $files[] = 'templates/' . $template->name . '/css/custom.css';
