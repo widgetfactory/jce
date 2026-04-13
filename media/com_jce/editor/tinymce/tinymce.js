4c23e8c1b5b0ebe85432e2a4ef828ccf88b70f8d
Ryan <ryan@widgetfactorylimited.co.uk>
2026-04-13 14:58:04 +0100

    Update tinymce.js

--- media/com_jce/editor/tinymce/tinymce.js
+++ media/com_jce/editor/tinymce/tinymce.js
@@ -21858,12 +21858,9 @@
           // remove empty spans
       } else {
           each$3(dom.select('span', o.node), function (n) {
-              // remove span without children eg: <span></span>
-              if (!n.hasChildNodes()) {
-                  // remove span without attributes
-                  if (dom.getAttribs(n).length === 0) {
-                      dom.remove(n, 1);
-                  }
+              // remove span without attributes eg: <span>foo</span> or <span></span>
+              if (dom.getAttribs(n).length === 0) {
+                  dom.remove(n, n.hasChildNodes() ? 1 : 0);
               }
           });
       }
@@ -48390,13 +48387,16 @@
         }
       };
 
-      // special quotes shortcute
+      // special quotes shortcut
       ed.onKeyUp.add(function (ed, e) {
         // eslint-disable-next-line dot-notation
         var map = quoteMap[ed.settings.language] || quoteMap['en'];
 
-        if ((e.key == '\u0022' || e.key == '\u0027') && e.shiftKey && e.ctrlKey) {
-          var value = map[e.key];
+        // Gate on the quote characters, then use e.shiftKey to disambiguate.
+        // e.key alone is unreliable: on macOS, Ctrl+' (no Shift) reports e.key as '"',
+        // so both shortcuts would produce double quotes. e.shiftKey is the stable signal.
+        if (e.ctrlKey && !e.altKey && (e.key == '\u0022' || e.key == '\u0027')) {
+          var value = map[e.shiftKey ? '\u0022' : '\u0027'];
 
           ed.undoManager.add();
           ed.execCommand('mceReplaceContent', false, value);
