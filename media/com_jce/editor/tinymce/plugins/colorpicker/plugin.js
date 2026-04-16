beb44615d8aa099b566f4840cdc80dd3640d796b
Ryan <ryan@widgetfactorylimited.co.uk>
2026-04-16 13:48:58 +0100

    Fix dialog size

--- media/com_jce/editor/tinymce/plugins/colorpicker/plugin.js
+++ media/com_jce/editor/tinymce/plugins/colorpicker/plugin.js
@@ -16,8 +16,7 @@
     ed.addCommand('mceColorPicker', function (ui, v) {
       ed.windowManager.open({
         url: ed.getParam('site_url') + 'index.php?option=com_jce&task=plugin.display&plugin=colorpicker',
-        width: 365,
-        height: 320,
+        size: 'mce-modal-portrait-320',
         close_previous: false
       }, {
         input_color: v.color,
