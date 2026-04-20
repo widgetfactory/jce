e5519441a3ca71ad73ee3f8a55c85d691ea6444a
Ryan <ryan@widgetfactorylimited.co.uk>
2026-04-20 15:32:40 +0100

    Fix $theme reference

--- plugins/editors/jce/Wfe/views/plugin/index.php
+++ plugins/editors/jce/Wfe/views/plugin/index.php
@@ -10,6 +10,8 @@
 
 \defined('_JEXEC') or die;
 
+$theme = $this->getConfig('theme');
+
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo $this->language; ?>" dir="<?php echo $this->direction; ?>">
@@ -18,7 +20,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<!-- [head] -->
 	</head>
-	<body lang="<?php echo $this->language; ?>" id="jce" class="uk-jce <?php echo $this->theme; ?> uk-form uk-form-horizontal" data-plugin="<?php echo $this->name; ?>">
+	<body lang="<?php echo $this->language; ?>" id="jce" class="uk-jce <?php echo $theme; ?> uk-form uk-form-horizontal" data-plugin="<?php echo $this->name; ?>">
 		<!-- [body] -->
 	</body>
 </html>
