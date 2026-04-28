0564b03c3958f6ca6eea24af450417b5be4186b4
Ryan <ryan@widgetfactorylimited.co.uk>
2026-04-28 20:13:58 +0100

    Update tinymce.js

--- media/com_jce/editor/tinymce/tinymce.js
+++ media/com_jce/editor/tinymce/tinymce.js
@@ -32058,8 +32058,7 @@
 
         // find and clear input element
         input.value = '';
-        input.focus();
-
+        
         if (removetags) {
           DOM.remove(DOM.select('.mceButtonTag', this.id));
         }
