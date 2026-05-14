ffb77b5fa0df8657d29a63f439b42431df74bc4b
Ryan <ryan@widgetfactorylimited.co.uk>
2026-05-14 16:12:47 +0100

    Refactor XTDButtonsTrait

--- plugins/editors/jce/src/PluginTraits/XTDButtonsTrait.php
+++ plugins/editors/jce/src/PluginTraits/XTDButtonsTrait.php
@@ -1,21 +1,19 @@
 <?php
+
 /**
  * @package     JCE
  * @subpackage  Editors.Jce
  *
  * @copyright   Copyright (C) 2005 - 2023 Open Source Matters, Inc. All rights reserved.
- * @copyright   Copyright (c) 2009-2026 Ryan Demmer. All rights reserved
+ * @copyright   Copyright (c) 2009-2024 Ryan Demmer. All rights reserved
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 namespace Joomla\Plugin\Editors\Jce\PluginTraits;
 
-use Joomla\CMS\Factory;
 use Joomla\CMS\Editor\Editor;
 use Joomla\CMS\Layout\LayoutHelper;
-use Joomla\CMS\Plugin\PluginHelper;
 use Joomla\CMS\Uri\Uri;
-use Joomla\Event\Event;
 
 // phpcs:disable PSR1.Files.SideEffects
 \defined('_JEXEC') or die;
@@ -23,13 +21,25 @@ use Joomla\Event\Event;
 
 trait XTDButtonsTrait
 {
-    private function getXtdButtonsList($name, $buttons, $asset, $author)
+    /**
+     * Get the XTD buttons as a list to render in the Joomla button menu
+     *
+     * @param   string  $editorId  The editor ID
+     * @param   array   $options   Associative array with additional parameters
+     *
+     * @return array
+     *
+     * @since 2.9.99.3
+     */
+    private function getXtdButtonsList($editorId, array $options = []): array
     {
-        $app = Factory::getApplication();
-        
-        $list = array();
+        $app = $this->getApplication();
 
-        $excluded = array('readmore', 'pagebreak');
+        $list = [];
+
+        $excluded = ['readmore', 'pagebreak'];
+
+        $buttons = $options['buttons'] ?? [];
 
         if (!is_array($buttons)) {
             $buttons = !$buttons ? false : $excluded;
@@ -37,86 +47,132 @@ trait XTDButtonsTrait
             $buttons = array_merge($buttons, $excluded);
         }
 
-        // easiest way to get buttons across versions
-        $buttons = Editor::getInstance('jce')->getButtons($name, $buttons);
+        $buttons = Editor::getInstance('jce')->getButtons($editorId, $buttons);
 
         if (!empty($buttons)) {
-            $list[$name] = array();
-            
-            foreach ($buttons as $i => $button) {
-                if ($button->get('name')) {
-                    $id = $name . '_' . $button->name;
+            $list[$editorId] = [];
 
-                    if (version_compare(JVERSION, '4', 'ge')) {
-                        $button->id = $id . '_modal';
-                        echo LayoutHelper::render('joomla.editors.buttons.modal', $button);
-                    }
+            $wa = $app->getDocument()->getWebAssetManager();
 
-                    // create icon class
-                    $icon = 'none icon-' . $button->get('icon', $button->get('name'));
+            foreach ($buttons as $button) {
 
-                    $options = (array) $button->get('options', array());
-
-                    // set href value
-                    if ($button->get('link', '#') == '#') {
-                        $link = isset($options['src']) ? $options['src'] : '';
-                    } else {
-                        $link = Uri::base() . $button->get('link');
-                    }
-
-                    // Joomla 5 modal requirements
-                    if ($button->get('action', '')) {
-                        $options['src']         = $link;
-                        $options['textHeader']  = $button->get('text');
-                        $options['iconHeader']  = 'icon-' . $icon;
-                        $options['popupType']   = $options['popupType'] ?? 'iframe';
-                    }
-
-                    $args = array(
-                        'name' => $button->get('text'),
-                        'id' => $id,
-                        'title' => $button->get('text'),
-                        'icon' => $icon,
-                        'href' => $link,
-                        'onclick' => $button->get('onclick', ''),
-                        'svg' => $button->get('iconSVG'),
-                        'options' => $options,
-                        'action' => $button->get('action', '')
-                    );
-
-                    $list[$name][] = $args;
+                if (!$button->get('name')) {
+                    continue;
                 }
+
+                $id             = $editorId . '_' . $button->get('name');
+                $action         = $button->get('action', '');
+                $legacyModal    = $button->get('modal');
+
+                $btnAsset       = 'editor-button.' . $button->get('name');
+
+                if ($wa->assetExists('style', $btnAsset)) {
+                    $wa->useStyle($btnAsset);
+                }
+                if ($wa->assetExists('script', $btnAsset)) {
+                    $wa->useScript($btnAsset);
+                }
+
+                $icon       = 'none icon-' . $button->get('icon', $button->get('name'));
+                $btnOptions = (array) $button->get('options', []);
+
+                $link    = $button->get('link', '#');
+
+                if ($link === '#') {
+                    $link = $btnOptions['src'] ?? '';
+                } else {
+                    $link               = str_contains($link, '&amp;') ? htmlspecialchars_decode($link) : $link;
+                    $link               = Uri::base(true) . '/' . $link;
+                    $btnOptions['src'] ??= $link;
+                }
+
+                if ($action === 'modal' && $wa->assetExists('script', 'joomla.dialog')) {
+                    $wa->useScript('joomla.dialog');
+
+                    $legacyModal = false;
+
+                    $btnOptions['src']        = $btnOptions['src'] ?? $link;
+                    $btnOptions['textHeader'] = $btnOptions['textHeader'] ?? $button->get('text');
+                    $btnOptions['iconHeader'] = $btnOptions['iconHeader'] ?? 'icon-' . $icon;
+                    $btnOptions['popupType']  = $btnOptions['popupType'] ?? 'iframe';
+                }
+
+                if ($legacyModal) {
+                    $button->id = $id . '_modal';
+                    echo LayoutHelper::render('joomla.editors.buttons.modal', $button);
+                }
+
+                $args = [
+                    'name'    => $button->get('text'),
+                    'id'      => $id,
+                    'title'   => $button->get('text'),
+                    'icon'    => $icon,
+                    'href'    => $link,
+                    'onclick' => $button->get('onclick', ''),
+                    'svg'     => $button->get('iconSVG'),
+                    'options' => $btnOptions,
+                    'action'  => $action,
+                ];
+
+                if ($legacyModal) {
+                    $args['bsModal'] = true;
+                }
+
+                $list[$editorId][] = $args;
             }
         }
 
         return $list;
     }
-
-    protected function displayXtdButtons($name, $buttons, $asset, $author, $hidden = false)
+    /**
+     * Display the extended buttons for the editor.
+     *
+     * @param   string  $editorId  The editor ID
+     * @param   array   $options   Associative array with additional parameters
+     *
+     * @return  string
+     */
+    protected function displayXtdButtons(string $editorId, array $options): string
     {
-        // easiest way to get buttons across versions
-        $buttons = Editor::getInstance('jce')->getButtons($name, $buttons);
+        $app = $this->getApplication();
+
+        $options['buttons'] ??= true;
+
+        $buttons = Editor::getInstance('jce')->getButtons($editorId, $options['buttons']);
 
         if (!empty($buttons)) {
-            // fix some buttons attributes
+            $wa = $app->getDocument()->getWebAssetManager();
+
             foreach ($buttons as $button) {
                 $cls = $button->get('class', '');
 
                 if (empty($cls) || strpos($cls, 'btn') === false) {
-                    $cls .= ' btn';
-                    $button->set('class', trim($cls));
+                    $button->set('class', trim($cls . ' btn'));
                 }
-                
-                // set the editor name (Joomla 5) so each modal is unique
-                $button->set('editor', $name);
 
-                // hide buttons if required
-                if ($hidden) {
+                $button->set('editor', $editorId);
+
+                if ($options['hidden'] ?? false) {
                     $button->set('class', 'd-none hidden');
+                }
+
+                $btnAsset = 'editor-button.' . $button->get('name');
+
+                if ($wa->assetExists('style', $btnAsset)) {
+                    $wa->useStyle($btnAsset);
+                }
+                if ($wa->assetExists('script', $btnAsset)) {
+                    $wa->useScript($btnAsset);
+                }
+
+                if ($button->get('action') === 'modal' && $wa->assetExists('script', 'joomla.dialog')) {
+                    $wa->useScript('joomla.dialog');
                 }
             }
 
             return LayoutHelper::render('joomla.editors.buttons', $buttons);
         }
+
+        return '';
     }
 }
