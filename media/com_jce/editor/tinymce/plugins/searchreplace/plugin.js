a30090de1ae7e2b2beba7c5bcf5449361decccd1
Ryan <ryan@widgetfactorylimited.co.uk>
2026-05-24 11:59:43 +0100

    Fix search & replace dialog closing scroll position to found match

--- media/com_jce/editor/tinymce/plugins/searchreplace/plugin.js
+++ media/com_jce/editor/tinymce/plugins/searchreplace/plugin.js
@@ -736,6 +736,10 @@
 
                 if (keepEditorSelection !== false) {
                     editor.selection.setRng(rng);
+                    var scrollTarget = startContainer.parentNode;
+                    window.setTimeout(function () {
+                        editor.selection.scrollIntoView(scrollTarget);
+                    }, 0);
                 }
 
                 return rng;
