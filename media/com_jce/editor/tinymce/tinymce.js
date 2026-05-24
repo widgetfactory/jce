2db3cca1cbdaad92e498c118358d47c6996d5155
Ryan <ryan@widgetfactorylimited.co.uk>
2026-05-24 12:01:42 +0100

    Update tinymce.js

--- media/com_jce/editor/tinymce/tinymce.js
+++ media/com_jce/editor/tinymce/tinymce.js
@@ -49873,7 +49873,7 @@
             if (node.name !== 'br') {
               continue;
             }
-
+            
             if (!node.prev && !node.next) {
               textNode = new Node('#text', 3);
               textNode.value = '\u00a0';
@@ -49934,12 +49934,6 @@
             }
           });
         }
-
-        if (!ed.getParam('table_pad_empty_cells', true)) {
-          elements.th.paddEmpty = false;
-          elements.td.paddEmpty = false;
-        }
-
         each(elements, function (v, k) {
           if (k.indexOf('mce:') === 0) {
             return true;
@@ -50011,10 +50005,6 @@
           if (!ed.getParam('remove_tag_padding')) {
             o.content = o.content.replace(/<(p|h1|h2|h3|h4|h5|h6|th|td|pre|div|address|caption)\b([^>]*)><\/\1>/gi, '<$1$2>&nbsp;</$1>');
           }
-        }
-
-        if (!ed.getParam('table_pad_empty_cells', true)) {
-          o.content = o.content.replace(/<(th|td)([^>]*)>(&nbsp;|\u00a0)<\/\1>/gi, '<$1$2></$1>');
         }
 
         o.content = o.content.replace(/<(a|i|span)([^>]+)>(&nbsp;|\u00a0)<\/\1>/gi, function (match, tag, attribs) {
