f595edc4bc310bfd462b05e0a5b5de4909bb6d4c
Ryan <ryan@widgetfactorylimited.co.uk>
2026-05-25 16:55:43 +0100

    Update install.pkg.php

--- administrator/components/com_jce/install.pkg.php
+++ administrator/components/com_jce/install.pkg.php
@@ -532,7 +532,8 @@ class pkg_jceInstallerScript implements DatabaseAwareInterface
         $files['3.0.0'] = array(
             $admin . '/controller.php',
             $admin . '/jce.php',
-            $admin . '/includes/classmap.php'
+            $admin . '/includes/classmap.php',
+            $site  . '/jce.php'
         );
 
         foreach ($folders as $version => $list) {
