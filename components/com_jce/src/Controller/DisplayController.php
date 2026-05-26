03db4141fe9b96be062a69177f4dc7844fadee2f
Ryan <ryan@widgetfactorylimited.co.uk>
2026-05-26 11:04:26 +0100

    Create DisplayController.php

---
+++ components/com_jce/src/Controller/DisplayController.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @package     Jce.Site
+ * @subpackage  com_jce
+ *
+ * @copyright   Copyright (C) 2026 Ryan Demmer. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Joomla\Component\Jce\Site\Controller;
+
+use Joomla\CMS\Access\Exception\NotAllowed;
+use Joomla\CMS\MVC\Controller\BaseController;
+
+// phpcs:disable PSR1.Files.SideEffects
+\defined('_JEXEC') or die;
+// phpcs:enable PSR1.Files.SideEffects
+
+class DisplayController extends BaseController
+{
+    public function display($cachable = false, $urlparams = []): void
+    {
+        throw new NotAllowed('Access denied', 403);
+    }
+}
