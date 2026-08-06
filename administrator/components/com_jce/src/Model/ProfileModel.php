b4c05e939399f60970553cd81cd241712847f8c0
Ryan <ryan@widgetfactorylimited.co.uk>
2026-08-06 15:48:38 +0100

    Redirect to the profiles list with an error message when a profile cannot be loaded

--- administrator/components/com_jce/src/Model/ProfileModel.php
+++ administrator/components/com_jce/src/Model/ProfileModel.php
@@ -291,6 +291,13 @@ class ProfileModel extends AdminModel
     {
         $data = $this->getItem();
 
+        // the requested profile could not be loaded
+        if ($data === false) {
+            $app = Factory::getApplication();
+            $app->enqueueMessage(Text::_('JLIB_APPLICATION_ERROR_NOT_EXIST'), 'error');
+            $app->redirect('index.php?option=com_jce&view=profiles');
+        }
+
         // convert 0 value to null to force defaults
         if (empty($data->area)) {
             $data->area = null;
