3936aa021dd2f559fd80da2f118a2d6e0fc5ae70
Ryan <ryan@widgetfactorylimited.co.uk>
2026-08-06 14:33:01 +0100

    Remove colorpicker and update tips.js

--- media/com_jce/admin/js/lib/core.js
+++ media/com_jce/admin/js/lib/core.js
@@ -1,6 +1,4 @@
 import '../../../editor/js/lib/plugin/tips.js';
-import '../../../editor/js/lib/plugin/colorpicker.js';
-
 import './core/keyvalue.js';
 import './core/repeatable.js';
 import './core/jce.js';
